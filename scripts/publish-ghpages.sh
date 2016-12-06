0c8ae0a2b31001e5e48ed0a0ddc1af6f3c25139f
patternfly-build <patternfly-build@redhat.com>
2016-12-06 07:13:52 -0800

    Added a logging statement when reading travis encrypted key vars

--- scripts/publish-ghpages.sh
+++ scripts/publish-ghpages.sh
@@ -61,7 +61,9 @@ getDeployKey () {
   # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
   ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
   ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+  echo "Checking Travis ENV VAR: ${ENCRYPTED_KEY_VAR}..."
   ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+  echo "Checking Travis ENV VAR: ${ENCRYPTED_IV_VAR}..."
   ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
   openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
   chmod 600 deploy_key
