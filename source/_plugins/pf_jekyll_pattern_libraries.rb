55add68fa66674d0d2825201c6e0b45c714afa64
patternfly-build <patternfly-build@redhat.com>
2017-01-18 14:17:27 -0800

    Extended the menu generation to re-format paths into proper titles

--- source/_plugins/pf_jekyll_pattern_libraries.rb
+++ source/_plugins/pf_jekyll_pattern_libraries.rb
@@ -1,21 +1,89 @@
 require "set"
 
 module Jekyll
-  class SitePatternLibraryGenerator < Jekyll::Generator
+  class PatternPage
+    attr_accessor :url, :library, :name, :title, :filename
+
+
+    def initialize(url)
+      @url = url
+      parts = url.split('/')
+      @library = parts[2]
+      @name = parts[3]
+      @filename = parts[4]
+      @title = generateTitle(@name)
+    end
+
+    def generateTitle(string)
+      string.split('-')
+        .each{|i| i.capitalize! if ! ['and', 'or'].include? i }
+        .join(' ') unless string.nil?
+    end
+
+    def ==(other)
+      self.url == other.url
+    end
+
+    def <=>(other)
+      self.url <=> other.url
+    end
+
+    def to_liquid
+      {
+        'url' => url,
+        'library' => library,
+        'name' => name,
+        'title' => title
+      }
+    end
+  end
+
+  class PatternLibrary
+    attr_accessor :library, :title
+    def initialize(url)
+      @url = url
+      parts = url.split('/')
+      @library = parts[2]
+      @title = generateTitle(@library)
+    end
+
+    def generateTitle(string)
+      string.split('-')
+        .each{|i| i.capitalize! if ! ['and', 'or'].include? i }
+        .join(' ') unless string.nil?
+    end
+
+    def ==(other)
+      self.library == other.library
+    end
+
+    def <=>(other)
+      self.library <=> other.library
+    end
+
+    def to_liquid
+      {
+        'library' => library,
+        'title' => title
+      }
+    end
+  end
+
+  class SitePatternPageGenerator < Jekyll::Generator
     def generate(site)
       libs = Array.new
       pages = Array.new
       site.pages.each do |page|
         parts = page.url.split('/')
         if ( parts[1] == 'pattern-library' && !parts[2].nil?)
-          lib = parts[2]
-          pages.push(page)
-          # lib = page.url
-          libs.push(lib) unless libs.include?(lib)
+          patternPage = PatternPage.new(page.url)
+          pages.push(patternPage) if patternPage.filename.nil?
+          patternLibrary = PatternLibrary.new(page.url)
+          libs.push(patternLibrary) unless libs.include?(patternLibrary)
         end
       end
       site.config['patternLibraries'] = libs.sort
-      site.config['patternPages'] = pages
+      site.config['patternPages'] = pages.sort
     end
   end
 end
