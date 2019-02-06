81de29afd4aadf28823c47a1f745f5e0a0620522
Joachim Schuler <jschuler@redhat.com>
2019-02-06 13:14:56 -0500

    prod

--- packages/patternfly-4/gatsby-browser.js
+++ packages/patternfly-4/gatsby-browser.js
@@ -9,13 +9,13 @@ require('./_repos/core/src/patternfly/patternfly.scss');
  * Display message when update is available, see:
  * https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates
  */
-exports.onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `A newer version of the site is available. ` +
-      `Reload to display the latest version?`
-  )
+// exports.onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `A newer version of the site is available. ` +
+//       `Reload to display the latest version?`
+//   )
 
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
