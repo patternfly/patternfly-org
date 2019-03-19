226d161e4d32646ec10519c70d11dd1bffc63c26
Joachim Schuler <jschuler@redhat.com>
2019-03-19 09:57:40 -0400

    css utilities

--- packages/patternfly-4/gatsby-browser.js
+++ packages/patternfly-4/gatsby-browser.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 require('./_repos/core/src/patternfly/patternfly.scss');
+require('./_repos/core/src/patternfly/patternfly-addons.scss');
 require('prismjs/themes/prism-coy.css');
 require('./src/workspace.scss');
 
