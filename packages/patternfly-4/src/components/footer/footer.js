e3de1d9aa5d14c3e9bde52f3e17e6d9d9cf58441
Adam Jolicoeur <mindreeper2420@gmail.com>
2019-03-25 12:10:16 -0400

    make footer separate component

---
+++ packages/patternfly-4/src/components/footer/footer.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import './footer.scss';
+import {
+  Grid,
+  GridItem,
+  Nav,
+  NavItem,
+  NavList,
+  PageSection,
+  Text, TextVariants,
+  Title
+} from '@patternfly/react-core';
+import patternflyLogo from '../../images/l_pf-reverse-164x11.png';
+import redhatLogo from '../../images/RHLogo.svg';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <PageSection className="pf4-l-footer">
+          <Grid className="pf-u-py-xl-on-sm pf-u-py-0-on-md">
+            <GridItem md={5} mdOffset={1} className="pf-u-mb-lg pf-u-mb-0-on-sm">
+              <Grid className="pf-u-py-xl-on-sm pf-u-py-8-on-md">
+                <GridItem span={6} sm={4}>
+                  <Title size="md" className="pf-m-white">QUICKLINKS</Title>
+                  <Nav aria-label="Quick Links" className="pf-m-white">
+                    <NavList className="pf-c-nav__list-footer">
+                      <NavItem to="/get-started" aria-label="Get started with PatternFly 4">
+                        Get started
+                      </NavItem>
+                      <NavItem to="/documentation/react/components/alert" aria-label="PatternFly 4 components">
+                        Components
+                      </NavItem>
+                      <NavItem to="/documentation/react/layouts/bullseye" aria-label="PatternFly 4 layouts">
+                        Layouts
+                      </NavItem>
+                      <NavItem to="design-guidelines/styles/icons" aria-label="PatternFly 4 styles">
+                        Styles
+                      </NavItem>
+                      <NavItem to="https://www.patternfly.org" target="top" aria-label="View the PatternFly 3 website">
+                        PatternFly 3
+                      </NavItem>
+                    </NavList>
+                  </Nav>
+                </GridItem>
+                <GridItem span={6} sm={4}>
+                  <Title size="md" className="pf-m-white">CONTRIBUTE</Title>
+                  <Nav aria-label="Quick Links">
+                    <NavList className="pf-c-nav__list-footer">
+                      <NavItem to="/get-started/designers" aria-label="How to contribute as a Designer">
+                        Designers
+                      </NavItem>
+                      <NavItem to="/get-started/developers" aria-label="How to contribute as a Developer">
+                        Developers
+                      </NavItem>
+                      <NavItem to="https://github.com/patternfly/patternfly/blob/master/CODE_OF_CONDUCT.md" target="top" aria-label="PatternFly 4 Code of Conduct">
+                        Code of Conduct
+                      </NavItem>
+                    </NavList>
+                  </Nav>
+                </GridItem>
+              </Grid>
+            </GridItem>
+            <GridItem md={5}>
+              <Grid className="pf-u-py-xl-on-sm pf-u-py-8-on-md">
+                <GridItem sm={12} md={8} mdOffset={4} className="pf4-l-footer-column pf-u-p-lg">
+                  <img src={patternflyLogo} alt="PatternFly logo" className="pf-u-pb-lg" />
+                  <p className="pf-m-white pf-u-pb-lg">
+                    PatternFly is an open source design system built to drive consistency and unify teams. We provide tools like design documentation, components, and code examples to make it possible for anyone to design and build responsive, accessible web applications.
+                  </p>
+                  <Text component={TextVariants.a} href="https://www.facebook.com/groups/PatternFlyCommunity/" target="top" aria-label="Link to PatternFly Facebook page">
+                    <i class="fab fa-facebook"></i>
+                  </Text>
+                  <Text component={TextVariants.a} href="https://github.com/patternfly/patternfly" target="top" aria-label="Link to PatternFly GitHub page">
+                    <i class="fab fa-github"></i>
+                  </Text>
+                  <Text component={TextVariants.a} href="https://twitter.com/patternfly_des" target="top" aria-label="Link to PatternFly Twitter page">
+                    <i class="fab fa-twitter"></i>
+                  </Text>
+                </GridItem>
+              </Grid>
+            </GridItem>
+          </Grid>
+        </PageSection>
+        <PageSection className="pf4-l-footer-dark">
+          <Grid className="pf-u-py-xl-on-sm pf-u-py-0-on-md pf-u-align-items-center">
+            <GridItem md={3} mdOffset={1}>
+              <Text component={TextVariants.a} href="https://www.redhat.com" target="top" aria-label="Visit Red Hat.com">
+                <img src={redhatLogo} alt="Red Hat logo" />
+              </Text>
+            </GridItem>
+            <GridItem md={2}>
+              Copyright &copy; 2019 Red Hat, Inc.
+            </GridItem>
+            <GridItem md={4}>
+              <Text component={TextVariants.a} href="https://www.redhat.com/en/about/privacy-policy" target="top" aria-label="Privacy statement">
+                Privacy Statement
+              </Text>
+              <Text component={TextVariants.a} href="https://www.redhat.com/en/about/terms-use" target="top" aria-label="Terms of use">
+                Terms of use
+              </Text>
+              <Text component={TextVariants.a} href="https://www.redhat.com/en/about/all-policies-guidelines" target="top" aria-label="All policies and guidelines">
+                All policies and guidelines
+              </Text>
+            </GridItem>
+          </Grid>
+        </PageSection>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- packages/patternfly-4/src/components/layout.js
+++ packages/patternfly-4/src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql, withPrefix, Link, navigate } from 'gatsby';
 import Header from './header';
+import Footer from './footer/footer';
 import {
   Brand,
   Nav,
@@ -123,6 +124,7 @@ class Layout extends React.Component {
               {tertiaryNav}
             </PageSection>}
             {this.props.children}
+            <Footer></Footer>
           </Page>
         </>
       )
