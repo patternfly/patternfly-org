0de7c47ae77ad4f9e629f7a80dedfb827c1ed690
Joachim Schuler <jschuler@redhat.com>
2019-04-10 15:08:42 -0400

    combined

--- packages/patternfly-4/gatsby-config.js
+++ packages/patternfly-4/gatsby-config.js
@@ -1,7 +1,7 @@
 const globImporter = require('node-sass-glob-importer');
 
 module.exports = {
-  pathPrefix: '/4.0',
+  pathPrefix: '/v4',
   siteMetadata: {
     title: 'PatternFly 4',
     description: 'Documentation for PatternFly 4',
@@ -157,5 +157,16 @@ module.exports = {
     //     directory: `${__dirname}/_repos/core`
     //   }
     // },
+    // { 
+    //   resolve: `gatsby-plugin-purgecss`,
+    //   options: {
+    //     printRejected: true, // Print removed selectors and processed file names
+    //     develop: true, // Enable while using `gatsby develop`
+    //     // tailwind: true, // Enable tailwindcss support
+    //     // whitelist: ['whitelist'], // Don't remove this selector
+    //     // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+    //     // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    //   }
+    // }
   ],
 }
