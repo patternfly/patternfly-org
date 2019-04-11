a83bb2c77403921b14d62de0edbf05d3505c6e9b
Joachim Schuler <jschuler@redhat.com>
2019-04-11 10:30:54 -0400

    move resolve in gatsby-node

--- packages/patternfly-4/gatsby-node.js
+++ packages/patternfly-4/gatsby-node.js
@@ -8,7 +8,6 @@ const path = require('path');
 const fs = require('fs-extra');
 const paramCase = require('param-case');
 const glob = require('glob');
-const ChildProcess = require('child_process');
 
 // Map to handlebars partial files for Core
 let partialsToLocationsMap = null;
@@ -215,8 +214,8 @@ exports.createPages = ({ graphql, actions }) => {
           context: {}, // additional data can be passed via context
         })
       });
+      resolve();
     });
-    resolve();
   })
 };
 
@@ -309,8 +308,4 @@ const continueWebpackConfig = ({ stage, loaders, actions, plugins, getConfig }) 
     }
     actions.replaceWebpackConfig(config);
   }
-};
-
-exports.onPostBuild = () => {
-  ChildProcess.execSync("ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill");
 };
