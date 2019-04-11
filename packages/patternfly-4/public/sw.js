3bdffc56ff9a49c1739fa00555a361b1a3f02373
Joachim Schuler <jschuler@redhat.com>
2019-04-11 17:10:21 -0400

    prebuild_cache

---
+++ packages/patternfly-4/public/sw.js
@@ -0,0 +1,177 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-fd7c9f7bac77de02cb0f.js"
+  },
+  {
+    "url": "styles.26474962bbd807f45012.css"
+  },
+  {
+    "url": "styles-3f71849823f83defe205.js"
+  },
+  {
+    "url": "app-07525482bd2ca08fca94.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-9d75f2c6bf5c1a3ad584.js"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "7a4b3fdcb2ac5d45ea62929516a7ce19"
+  },
+  {
+    "url": "1-2c20d1539cf1d6ed2562.js"
+  },
+  {
+    "url": "component---src-pages-404-js-b2a39178c83086b1d85c.js"
+  },
+  {
+    "url": "8-6f3e6d64bf8283091c31.js"
+  },
+  {
+    "url": "3-6ed8adce9a034e44d943.js"
+  },
+  {
+    "url": "static/d/285/path---404-html-516-62a-0SUcWyAf8ecbYDsMhQkEfPzV8.json"
+  },
+  {
+    "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "3310d1149aa5cc5056d61df745a82658"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
+
+/* global importScripts, workbox, idbKeyval */
+
+importScripts(`idb-keyval-iife.min.js`)
+const WHITELIST_KEY = `custom-navigation-whitelist`
+
+const navigationRoute = new workbox.routing.NavigationRoute(({ event }) => {
+  const { pathname } = new URL(event.request.url)
+
+  return idbKeyval.get(WHITELIST_KEY).then((customWhitelist = []) => {
+    // Respond with the offline shell if we match the custom whitelist
+    if (customWhitelist.includes(pathname)) {
+      const offlineShell = `/v4/offline-plugin-app-shell-fallback/index.html`
+      const cacheName = workbox.core.cacheNames.precache
+
+      return caches.match(offlineShell, { cacheName }).then(cachedResponse => {
+        if (cachedResponse) return cachedResponse
+
+        console.error(
+          `The offline shell (${offlineShell}) was not found ` +
+            `while attempting to serve a response for ${pathname}`
+        )
+
+        return fetch(offlineShell).then(response => {
+          if (response.ok) {
+            return caches.open(cacheName).then(cache =>
+              // Clone is needed because put() consumes the response body.
+              cache.put(offlineShell, response.clone()).then(() => response)
+            )
+          } else {
+            return fetch(event.request)
+          }
+        })
+      })
+    }
+
+    return fetch(event.request)
+  })
+})
+
+workbox.routing.registerRoute(navigationRoute)
+
+let updatingWhitelist = null
+
+function rawWhitelistPathnames(pathnames) {
+  if (updatingWhitelist !== null) {
+    // Prevent the whitelist from being updated twice at the same time
+    return updatingWhitelist.then(() => rawWhitelistPathnames(pathnames))
+  }
+
+  updatingWhitelist = idbKeyval
+    .get(WHITELIST_KEY)
+    .then((customWhitelist = []) => {
+      pathnames.forEach(pathname => {
+        if (!customWhitelist.includes(pathname)) customWhitelist.push(pathname)
+      })
+
+      return idbKeyval.set(WHITELIST_KEY, customWhitelist)
+    })
+    .then(() => {
+      updatingWhitelist = null
+    })
+
+  return updatingWhitelist
+}
+
+function rawResetWhitelist() {
+  if (updatingWhitelist !== null) {
+    return updatingWhitelist.then(() => rawResetWhitelist())
+  }
+
+  updatingWhitelist = idbKeyval.set(WHITELIST_KEY, []).then(() => {
+    updatingWhitelist = null
+  })
+
+  return updatingWhitelist
+}
+
+const messageApi = {
+  whitelistPathnames(event) {
+    let { pathnames } = event.data
+
+    pathnames = pathnames.map(({ pathname, includesPrefix }) => {
+      if (!includesPrefix) {
+        return `/v4${pathname}`
+      } else {
+        return pathname
+      }
+    })
+
+    event.waitUntil(rawWhitelistPathnames(pathnames))
+  },
+
+  resetWhitelist(event) {
+    event.waitUntil(rawResetWhitelist())
+  },
+}
+
+self.addEventListener(`message`, event => {
+  const { gatsbyApi } = event.data
+  if (gatsbyApi) messageApi[gatsbyApi](event)
+})
---
+++ packages/patternfly-4/scripts/netlify-cache.js
@@ -0,0 +1,73 @@
+const { join, resolve, basename } = require('path');
+const { ensureDir, remove, readdir, copySync } = require('fs-extra');
+
+async function calculateDirs() {
+  const program = {
+    directory: join('/', 'opt', 'build', 'cache', 'packages', 'patternfly-4'),
+  };
+
+  const dirsToCache = [resolve(program.directory, 'public'), resolve(program.directory, '.cache')];
+
+  for (const dir of dirsToCache) {
+    await ensureDir(dir);
+  }
+
+  const netlifyCacheDir = resolve(process.env.NETLIFY_BUILD_BASE, 'cache', 'gatsby');
+
+  await ensureDir(netlifyCacheDir);
+
+  return {
+    dirsToCache,
+    netlifyCacheDir,
+  };
+}
+
+async function main() {
+  console.log('\nPrebuild - Checking Netlify server\n');
+
+  if (!process.env.NETLIFY_BUILD_BASE) {
+    console.log('\nNetlify Cache Post build - not on netlify server\n');
+    return;
+  }
+
+  const removePath = join('/', 'opt', 'build', 'cache');
+  remove(removePath, err => {
+    if (err) return console.error(err);
+    console.log('successfully removed cache dir'); // I just deleted my entire HOME directory.
+  });
+
+  const { dirsToCache, netlifyCacheDir } = await calculateDirs();
+
+  for (const dirPath of dirsToCache) {
+    const dirName = basename(dirPath);
+    const cachePath = resolve(netlifyCacheDir, dirName);
+
+    const dirFiles = await readdir(dirPath);
+    const cacheFiles = await readdir(cachePath);
+
+    console.log(
+      `Found ${dirFiles.length} files in ${dirName} directory with ${
+        cacheFiles.length
+      } already cached files.`,
+    );
+
+    try {
+      copySync(dirPath, cachePath);
+      console.log(
+        'NETLIFY CACHE PLUGIN - POST BUILD:\n Success - copied ',
+        cachePath,
+        ' to ',
+        dirPath,
+      );
+    } catch (err) {
+      console.log(
+        'NETLIFY CACHE PLUGIN - POST BUILD:\n Failed - could not copy ',
+        cachePath,
+        ' to ',
+        dirPath,
+      );
+    }
+  }
+}
+
+main();
