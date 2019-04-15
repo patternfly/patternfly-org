d4c974461962b8eb7615a031f3c33d7675bed026
Joachim Schuler <jschuler@redhat.com>
2019-04-15 16:13:54 -0400

    add banners

--- scripts/combine-sites.js
+++ scripts/combine-sites.js
@@ -5,6 +5,8 @@ const fs = require('fs-extra');
 const pf3_build = 'packages/patternfly-3/_site/.';
 const pf4_build = 'packages/patternfly-4/public/.';
 const build_root = 'out/';
+// const pf3_root = 'out/v3/';
+const pf3_root = build_root;
 const pf4_root = 'out/v4/';
 
 fs.remove(build_root, (errRemove) => {
@@ -24,16 +26,6 @@ fs.remove(build_root, (errRemove) => {
     // eslint-disable-next-line no-console
     console.log(`Created ${pf4_root} dir`);
 
-    // Copy PF3 files into /
-    fs.copy(pf3_build, build_root, (errPf3) => {
-      if (errPf3) {
-        // eslint-disable-next-line no-console
-        return console.error(errPf3);
-      }
-      // eslint-disable-next-line no-console
-      console.log(`Copied pf3 build into ${build_root}`);
-    });
-
     // Copy PF4 files into /v4
     fs.copy(pf4_build, pf4_root, (errPf4) => {
       if (errPf4) {
@@ -43,6 +35,24 @@ fs.remove(build_root, (errRemove) => {
       // eslint-disable-next-line no-console
       console.log(`Copied pf4 build into ${pf4_root}`);
     });
+  });
 
+  fs.ensureDir(pf3_root, (errMakeDir) => {
+    if (errMakeDir) {
+      // eslint-disable-next-line no-console
+      return console.error(errMakeDir);
+    }
+    // eslint-disable-next-line no-console
+    console.log(`Created ${pf3_root} dir`);
+
+    // Copy PF3 files into /
+    fs.copy(pf3_build, pf3_root, (errPf3) => {
+      if (errPf3) {
+        // eslint-disable-next-line no-console
+        return console.error(errPf3);
+      }
+      // eslint-disable-next-line no-console
+      console.log(`Copied pf3 build into ${build_root}`);
+    });
   });
 });
