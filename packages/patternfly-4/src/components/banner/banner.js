b72dab1e590fc075db964dca5bc56ddc66030b59
Christie Molloy <cmolloy@redhat.com>
2019-04-16 11:42:36 -0400

    updates to pf3 and pf4 banner

--- packages/patternfly-4/src/components/banner/banner.js
+++ packages/patternfly-4/src/components/banner/banner.js
@@ -5,8 +5,8 @@ import { TimesIcon } from '@patternfly/react-icons';
 const Banner = ({ onClose }) => (
   <div class="pf-l-flex pf-m-justify-content-space-between">
     <div class="pf-u-my-md">
-    <span class="pf-u-ml-xl custom-text-hide">Looking for All documentation and code examples are still available.</span>
-    <a href="/" style={{fontWeight: 900, textDecoration: 'none'}} class="pf-u-mx-md">Go to PatternFly 3<i class="fas fa-arrow-right pf-u-mx-sm"></i></a>
+    <span class="pf-u-ml-xl custom-text-hide">Looking for PatternFly 3? All documentation and code examples are still available.</span>
+    <a href="/" class="custom-text-link pf-u-mx-md">Go to PatternFly 3<i class="fas fa-arrow-right pf-u-mx-sm"></i></a>
     </div>
     {/* <div class="pf-u-mr-xl custom-close-button">
       <Button variant="plain" aria-label="Action" onClick={onClose}>
