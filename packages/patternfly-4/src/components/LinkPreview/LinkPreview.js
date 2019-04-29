469ab1d964706f147ebe7bcf1416533c2c018870
redallen <zallen@redhat.com>
2019-04-29 16:39:12 -0400

    fix(react): fullscreen examples

--- packages/patternfly-4/src/components/LinkPreview/LinkPreview.js
+++ packages/patternfly-4/src/components/LinkPreview/LinkPreview.js
@@ -1,44 +1,57 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { Location } from '@reach/router';
 import Img from 'gatsby-image';
 import './LinkPreview.scss';
 
-const LinkPreview = ({ data, name, path }) => {
-  const fileNameFromUrl = path.replace('/v4', '')
-    .replace(/\//g, '!')
-    .replace(/\?/g, '!__')
-    .replace(/\s/g, '_')
-    .replace(/%20/g, '_');
-  const previewScreenshot = data ? data.allFile.edges.filter(({ node }) => node.relativePath === `${fileNameFromUrl}.png`) : null;
-  return (
-  <>
-    {previewScreenshot && previewScreenshot.length ? (
-      <div className="Preview__body">
-        <a href={path} target="_blank" rel="noopener noreferrer">
-          <div className="preview-container">
-            <Img
-              fluid={previewScreenshot[0].node.childImageSharp.fluid}
-              alt={name}
-              className="preview-image"
-            />
-            <div className="preview-overlay">
-              <div className="preview-icon">
-                <i className="fas fa-external-link-alt"></i> 
+const encodePath = path => path.replace('/v4', '')
+  .replace(/\//g, '!')
+  .replace(/\?/g, '!__')
+  .replace(/\s/g, '_')
+  .replace(/%20/g, '_');
+
+const LinkPreview = ({ data, name, path }) => (
+  <Location>
+    {({ location }) => {
+      let fileNameFromUrl = encodePath(path);
+      if (fileNameFromUrl === path) {
+        // It's a PF4 md page
+        fileNameFromUrl = encodePath(location.pathname + path);
+      }
+      const previewScreenshot = data ? data.allFile.edges.filter(({ node }) => node.relativePath === `${fileNameFromUrl}.png`) : null;
+      if (previewScreenshot && previewScreenshot.length > 0) {
+        return (
+          <div className="Preview__body">
+            <a href={path} target="_blank" rel="noopener noreferrer">
+              <div className="preview-container">
+                <Img
+                  fluid={previewScreenshot[0].node.childImageSharp.fluid}
+                  alt={name}
+                  className="preview-image"
+                />
+                <div className="preview-overlay">
+                  <div className="preview-icon">
+                    <i className="fas fa-external-link-alt"></i> 
+                  </div>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
-        </a>
-      </div>
-    ) : (
-      <div className="Preview__body">
-        This Preview can only be accessed in&nbsp;
-        <a href={path} target="_blank" rel="noopener noreferrer">
-          full page mode
-        </a>.
-      </div>
-    )}
-  </>
-)};
+        );
+      } else {
+        return (
+          <div className="Preview__body">
+            This Preview can only be accessed in&nbsp;
+            <a href={path} target="_blank" rel="noopener noreferrer">
+              full page mode
+            </a>.
+          </div>
+        );
+      }
+    }
+  }
+  </Location>
+);
 
 export default props => (
   <StaticQuery
