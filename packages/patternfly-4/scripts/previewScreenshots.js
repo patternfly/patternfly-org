903b9908c0395091c8a12014d0130492ab5282f7
Joachim <jschuler@redhat.com>
2019-05-02 22:16:48 -0400

    Preview images update (#1054)
    * update preview images
    
    * cleanup

--- packages/patternfly-4/scripts/previewScreenshots.js
+++ packages/patternfly-4/scripts/previewScreenshots.js
@@ -5,39 +5,31 @@ const fs = require('fs');
 const path = require('path');
 const src = path.resolve(__dirname, './preview-screenshots.json');
 const previews = JSON.parse(fs.readFileSync(src));
-const gatsbyConfig = require('../gatsby-config');
 
 const options = {
-	width: 1280,
-  height: 800,
-  overwrite: true, 
-  element: '#___gatsby > div',
-  scaleFactor: 2,
-  fullPage: true,
-  delay: 1
-};
-
-const backgroundOptions = {
   width: 1280,
   height: 800,
   overwrite: true, 
   waitForElement: '#___gatsby > div',
   scaleFactor: 2,
-  delay: 1
+  delay: 1,
+  overwrite: true
 };
 
 function fileNameFromUrl(url) {
   return url
+    .replace('?', '/?')
     .replace('patternfly-4/', 'documentation/react/')
     .replace(/http.*\.(org|sh)/, '')
     .replace(/\//g, '!');
 }
 
 (async () => {
-  await Promise.all(previews[0].map(url => {
-		return captureWebsite.file(url, `previews/${fileNameFromUrl(url)}.png`, options);
-  }));
-  await Promise.all(previews[1].map(url => {
-		return captureWebsite.file(url, `previews/${fileNameFromUrl(url)}.png`, backgroundOptions);
+  await Promise.all(previews.paths.map(path => {
+    const combinedOptions = {
+      ...options,
+      ...path.options
+    };
+		return captureWebsite.file(path.url, `previews/${fileNameFromUrl(path.url)}.png`, combinedOptions);
 	}));
 })();
--- packages/patternfly-4/src/components/LinkPreview/LinkPreview.js
+++ packages/patternfly-4/src/components/LinkPreview/LinkPreview.js
@@ -10,7 +10,7 @@ import './LinkPreview.scss';
 const LinkPreview = ({ data, name, path }) => {
   const encodePath = path => path
     .replace(data.site.pathPrefix, '')
-    .replace(/\?/g, '!?')
+    .replace('?', '/?')
     .replace(/ /g, '%20')
     .replace(/\//g, '!');
 
@@ -22,7 +22,7 @@ const LinkPreview = ({ data, name, path }) => {
           // It's a PF4 md page, clearly too short
           fileNameFromUrl = encodePath(location.pathname + path + '/');
         }
-        const previewScreenshot = data ? data.allFile.edges.filter(({ node }) => node.relativePath === `${fileNameFromUrl}.png`) : null;
+        const previewScreenshot = data ? data.allFile.edges.filter(({ node }) => node.relativePath.indexOf(fileNameFromUrl) > -1) : null;
         if (previewScreenshot && previewScreenshot.length > 0) {
           return (
             <div className="Preview__body">
