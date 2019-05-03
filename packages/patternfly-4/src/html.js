4c40ad97774a66dd82cd2680492fe489d93deffd
Dana Gutride <dgutride@redhat.com>
2019-05-02 22:33:15 -0400

    fix(ref): close tag

--- packages/patternfly-4/src/html.js
+++ packages/patternfly-4/src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="shortcut icon" href="/v4/favicon.ico">
+        <link rel="shortcut icon" href="/v4/favicon.ico" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
