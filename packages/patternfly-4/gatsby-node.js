7964e6042b1a0a476dfc417170d2eace57fcf9c2
Joachim <jschuler@redhat.com>
2019-05-06 22:08:55 -0400

    redirects should end with spash (#1077)

--- packages/patternfly-4/gatsby-node.js
+++ packages/patternfly-4/gatsby-node.js
@@ -39,15 +39,15 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
   const redirects = [
-    { f: '/get-started', t: '/get-started/about' },
-    { f: '/contribute', t: '/contribute/about' },
-    { f: '/design-guidelines', t: '/design-guidelines/styles/colors' },
-    { f: '/design-guidelines/styles', t: '/design-guidelines/styles/colors' },
-    { f: '/documentation/core', t: '/documentation/core/components/aboutmodalbox'},
-    { f: '/documentation/react', t: '/documentation/react/components/aboutmodal'},
-    { f: '/documentation/react/components', t: '/documentation/react/components/aboutmodal'},
-    { f: '/documentation/react/layouts', t: '/documentation/react/layouts/bullseye'},
-    { f: '/documentation/react/demos', t: '/documentation/react/demos/pagelayout'}
+    { f: '/get-started', t: '/get-started/about/' },
+    { f: '/contribute', t: '/contribute/about/' },
+    { f: '/design-guidelines', t: '/design-guidelines/styles/colors/' },
+    { f: '/design-guidelines/styles', t: '/design-guidelines/styles/colors/' },
+    { f: '/documentation/core', t: '/documentation/core/components/aboutmodalbox/'},
+    { f: '/documentation/react', t: '/documentation/react/components/aboutmodal/'},
+    { f: '/documentation/react/components', t: '/documentation/react/components/aboutmodal/'},
+    { f: '/documentation/react/layouts', t: '/documentation/react/layouts/bullseye/'},
+    { f: '/documentation/react/demos', t: '/documentation/react/demos/pagelayout/'}
   ];
   redirects.forEach(({ f, t }) => {
     actions.createRedirect({
