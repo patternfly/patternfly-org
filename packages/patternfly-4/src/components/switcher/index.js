70edfa9379401568d5c4007d20851403c6d86809
Christie Molloy <cmolloy@redhat.com>
2019-05-15 11:28:40 -0400

    add class in

--- packages/patternfly-4/src/components/switcher/index.js
+++ packages/patternfly-4/src/components/switcher/index.js
@@ -10,6 +10,77 @@ import {
 import { Link } from 'gatsby';
 import { Location } from '@reach/router';
 import './styles.scss';
+import { path } from '../../../../../node_modules/change-case';
+
+class SingleSelectInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = [
+      { value: 'React', disabled: false, isPlaceholder: true},
+      { value: 'HTML', disabled: false },
+    ];
+
+    this.state = {
+      isExpanded: false,
+      selected: null
+    };
+
+    this.onToggle = isExpanded => {
+      this.setState({
+        isExpanded
+      });
+    };
+
+    this.onSelect = (event, selection, isPlaceholder, validPath) => {
+      if (isPlaceholder) this.clearSelection();
+      else {
+        this.setState({
+          selected: selection,
+          isExpanded: false,
+        });
+        console.log('selected:', selection, validPath);
+      }
+    };
+
+    this.clearSelection = () => {
+      this.setState({
+        selected: null,
+        isExpanded: false
+      });
+    };
+  }
+
+  render() {
+    const { isExpanded, selected } = this.state;
+    const titleId = 'title-id';
+    return (
+      <div>
+        <span id={titleId} hidden>
+          Title
+        </span>
+        <Select
+          variant={SelectVariant.single}
+          aria-label="Select Input"
+          onToggle={this.onToggle}
+          onSelect={this.onSelect}
+          selections={selected}
+          isExpanded={isExpanded}
+          ariaLabelledBy={titleId}
+        >
+          {this.options.map((option, index) => (
+            <SelectOption
+              isDisabled={option.disabled}
+              key={index}
+              value={option.value}
+              isPlaceholder={option.isPlaceholder}
+            >
+            </SelectOption>
+          ))}
+        </Select>
+      </div>
+    );
+  }
+}
 
 const trimTrailingSlash = str => str.replace(/\/$/, '');
 const verifyPath = (path, pages) =>
@@ -30,12 +101,9 @@ const Switcher = ({ data }) => (
       return (
         <Bullseye className="pf-site-switcher-group pf-u-ml-lg">
           <Title className="pf-site-switcher-group__title pf-u-mb-sm">FRAMEWORK</Title>
-          <div>
-            <Select className="pf-select-components" variant={SelectVariant.single}>
-              <SelectOption isDisabled={false} key={0} to={validReactPath} value={'React'} isActive={activeReact}></SelectOption>
-              <SelectOption isDisabled={false} key={1} to={validCorePath} value={'HTML'} isActive={activeCore}></SelectOption>
-            </Select>
-          </div>
+          <SingleSelectInput>
+            <SelectOption/>
+          </SingleSelectInput>
         </Bullseye>
       )
     }}
