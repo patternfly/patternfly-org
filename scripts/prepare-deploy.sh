3eea2719df79b3a282a8ed361e02620a9bee58a3
redallen <zallen@redhat.com>
2019-05-20 16:33:50 -0400

    Try new deploy

---
+++ scripts/prepare-deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env/bash
+
+BUILD_DIR=${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}
+find out -type d -print | xargs rmdir 2>/dev/null
+cp -r out $BUILD_DIR
+mv $BUILD_DIR out/travis-builds # Save a copy for history
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
