37018cdcb7a8c11890c23e98fd8ce9cec811fe7a
redallen <zallen@redhat.com>
2019-05-21 09:22:53 -0400

    fix credentials

--- scripts/prepare-deploy.sh
+++ scripts/prepare-deploy.sh
@@ -2,6 +2,6 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${AWS_ACCESS_KEY}
+aws_secret_access_key = ${AWS_SECRET_KEY}
 EOL
