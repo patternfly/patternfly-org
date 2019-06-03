ff0bfca505aab612f8b9a9a470d254697dfec502
Joachim <jschuler@redhat.com>
2019-06-03 10:30:08 -0400

    mobile app background and splash colors (#1165)

--- packages/patternfly-4/gatsby-config.js
+++ packages/patternfly-4/gatsby-config.js
@@ -134,9 +134,9 @@ module.exports = {
         // The start_url tells the browser where your application should start when it is launched
         start_url: '/v4',
         // The background_color property is used on the splash screen when the application is first launched
-        background_color: '#663399',
+        background_color: '#151515',
         // The theme_color sets the color of the tool bar, and may be reflected in the app's preview in task switchers.
-        theme_color: '#663399',
+        theme_color: '#151515',
         // Opens the web app to look and feel like a standalone native app
         display: 'standalone',
         // icon used for splash screen, on home screen
