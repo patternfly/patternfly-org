78aed29e837b9c963fb067f63287827d4d1e26e3
Joachim Schuler <jschuler@redhat.com>
2019-07-02 14:26:58 -0400

    fix core fullscreen urls

--- packages/patternfly-4/src/components/_core/Example/index.js
+++ packages/patternfly-4/src/components/_core/Example/index.js
@@ -26,25 +26,7 @@ export default class Example extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { isFull: false, viewport: '', fullPath: '', lights: true };
-  }
-
-  componentDidMount() {
-    let showComponent = true;
-
-    if (window.location.search !== '') {
-      // specific component was requested - make sure it matches
-      const queryObject = Example.parseQueryString(window.location.search.substr(1));
-      if (queryObject.component !== encodeURI(this.props.heading)) {
-        showComponent = false;
-      }
-    }
-
-    // eslint-disable-next-line react/no-did-mount-set-state
-    this.setState({
-      isFull: window.location.pathname.indexOf('-full') > 0,
-      showComponent
-    });
+    this.state = { viewport: '', lights: true };
   }
 
   onViewportChange = viewport => {
@@ -64,6 +46,20 @@ export default class Example extends React.Component {
       fullPageOnly,
       minHeight
     } = this.props;
+    if (typeof window !== 'undefined' && window.location.search.length) {
+      // requesting full screen view of an example
+      const queryObject = Example.parseQueryString(window.location.search.substr(1));
+      if (queryObject.component !== encodeURI(this.props.heading)) {
+        return null;
+      } else {
+        return (
+          <Preview className={className} heading={heading} viewport={this.state.viewport} lights={this.state.lights} fullPageOnly={fullPageOnly} minHeight={minHeight}>
+            {children}
+          </Preview>
+        );
+      }
+    }
+
     const sourceOutput =
       typeof children === 'string' ? children : ReactDOMServer.renderToStaticMarkup(children).replace(/ "/g, '"');
     const indentedOutput = pretty(sourceOutput, { ocd: true });
@@ -83,21 +79,16 @@ export default class Example extends React.Component {
     );
     const endsWithSlash = typeof window !== 'undefined' && window.location.href.substr(-1) === '/';
     const fullPath = typeof window !== 'undefined' && `${window.location.href.substr(0, window.location.href.length - (endsWithSlash ? 1 : 0))}-full/?component=${heading}`;
-    if (!this.state.isFull) {
-      return (
-        <div className="ws-live-demo">
-          <AutoLinkHeader size="lg" is="h4" className="ws-example-heading">{heading}</AutoLinkHeader>
-          {Boolean(description) && <p className="pf-c-content" dangerouslySetInnerHTML={{ __html: description }} />}
-          <Preview className={className} heading={heading} viewport={this.state.viewport} lights={this.state.lights} fullPageOnly={fullPageOnly} minHeight={minHeight}>
-            {children}
-          </Preview>
-          <ComponentItems children={children} className={css('example')} />
-          <EditorToolbar editor={editor} raw={indentedOutput} live={false} showMessage={false} fullPath={fullPath} showLights={false} onLightsChange={this.onLightsChange}/>
-        </div>
-      );
-    } else if (this.state.showComponent) {
-      return <div dangerouslySetInnerHTML={{ __html: this.props.children }} />;
-    }
-    return null;
+    return (
+      <div className="ws-live-demo">
+        <AutoLinkHeader size="lg" is="h4" className="ws-example-heading">{heading}</AutoLinkHeader>
+        {Boolean(description) && <p className="pf-c-content" dangerouslySetInnerHTML={{ __html: description }} />}
+        <Preview className={className} heading={heading} viewport={this.state.viewport} lights={this.state.lights} fullPageOnly={fullPageOnly} minHeight={minHeight}>
+          {children}
+        </Preview>
+        <ComponentItems children={children} className={css('example')} />
+        <EditorToolbar editor={editor} raw={indentedOutput} live={false} showMessage={false} fullPath={fullPath} showLights={false} onLightsChange={this.onLightsChange}/>
+      </div>
+    );
   }
 }
--- packages/patternfly-4/src/components/_core/Preview/index.js
+++ packages/patternfly-4/src/components/_core/Preview/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Location } from '@reach/router';
 import LinkPreview from '../../LinkPreview';
 import ShadowDomPreview from '../../ShadowDomPreview';
 import { CoreContext } from '../Documentation';
@@ -8,47 +7,35 @@ import './styles.scss';
 export default class Preview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      fullPath: ''
-    };
-  }
-
-  componentDidMount() {
-    // eslint-disable-next-line react/no-did-mount-set-state
-    const endsWithSlash = window.location.pathname.substr(-1) === '/';
-    this.setState({
-      fullPath: `${window.location.pathname.substr(0, window.location.pathname.length - (endsWithSlash ? 1 : 0))}-full?component=${
-        this.props.heading
-      }`
-    });
   }
 
   render() {
     const { 
       children, 
       fullPageOnly, 
-      isViewport, 
       viewport = '', 
       lights = true, 
       minHeight, 
       heading, 
-      raw, 
-      id,
       className
     } = this.props;
-    const { fullPath } = this.state;
+    const endsWithSlash = typeof window !== 'undefined' && window.location.pathname.substr(-1) === '/';
+    const fullPath = typeof window !== 'undefined' && `${window.location.pathname.substr(0, window.location.pathname.length - (endsWithSlash ? 1 : 0))}-full?component=${this.props.heading}`;
     const output = { __html: children };
     const background = lights ? '' : 'pf-t-dark pf-m-opaque-200';
     let preview;
-    if (raw) {
-      preview = (
-        <Location>
-          {({ location }) => <LinkPreview name={`Component ${id}`} path={`${location.pathname}fullscreen`} />}
-        </Location>
-      );
-    } else if (fullPageOnly) {
-      preview = <LinkPreview name={heading} path={fullPath} />;
-    } else {
+
+    if (typeof window !== 'undefined' && window.location.search.length) {
+      preview = <div>
+        <CoreContext.Consumer>
+          {({ coreClass }) => (
+            <ShadowDomPreview className={`${background} ${coreClass} ${className}`}>
+              <div dangerouslySetInnerHTML={output} />
+            </ShadowDomPreview>
+          )}
+        </CoreContext.Consumer>
+      </div>;
+    } else if (!fullPageOnly) {
       preview = <div>
         <CoreContext.Consumer>
           {({ coreClass }) => (
@@ -60,6 +47,8 @@ export default class Preview extends React.Component {
           )}
         </CoreContext.Consumer>
       </div>;
+    } else {
+      preview = <LinkPreview name={heading} path={fullPath} />;
     }
 
     return (
