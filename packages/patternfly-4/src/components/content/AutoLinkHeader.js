295a14616315b092a803d8b9145ac7eaa4008809
Joachim Schuler <jschuler@redhat.com>
2019-07-29 14:12:01 -0400

    add anchor links to two headings

--- packages/patternfly-4/src/components/content/AutoLinkHeader.js
+++ packages/patternfly-4/src/components/content/AutoLinkHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Title } from '@patternfly/react-core';
 import { GoLink } from 'react-icons/go';
 
-const slugger = (children, id) => {
+const slugger = children => {
   const value = Array.isArray(children) ? children.join('') : children;
   const whitespace = /\s/g;
   const specials = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g;
@@ -13,15 +13,15 @@ const slugger = (children, id) => {
     .replace(whitespace, '-');
 };
 
-const AutoLinkHeader = ({ is: Component, children, id, size, anchorOnly, ...props }) => {
-  const slug = slugger(children, id);
+const AutoLinkHeader = ({ is: Component, children, text, id, size, anchorOnly, floatAnchor, ...props }) => {
+  const slug = slugger(text || children);
   if (anchorOnly) {
     return <a href={`#${slug}`} {...props}>{children}</a>
   }
   if (!size) {
     return (
       <Component id={id || slug} {...props}>
-          <a href={`#${slug}`} className="anchor">
+          <a href={`#${slug}`} className={floatAnchor ? 'anchor' : 'anchor-no-float'}>
           <GoLink size="16" style={{ verticalAlign: 'middle' }} />
         </a>
         {children}
@@ -30,7 +30,7 @@ const AutoLinkHeader = ({ is: Component, children, id, size, anchorOnly, ...prop
   }
   return (
     <Title size={size} id={id || slug} headingLevel={Component} {...props}>
-      <a href={`#${slug}`} className="anchor">
+      <a href={`#${slug}`} className={floatAnchor ? 'anchor' : 'anchor-no-float'}>
         <GoLink size="16" style={{ verticalAlign: 'middle' }} />
       </a>
       {children}
@@ -38,6 +38,6 @@ const AutoLinkHeader = ({ is: Component, children, id, size, anchorOnly, ...prop
   )
 };
 
-AutoLinkHeader.defaultProps = { is: 'h2' };
+AutoLinkHeader.defaultProps = { is: 'h2', floatAnchor: true };
 
 export default AutoLinkHeader;
