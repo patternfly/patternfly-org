cb42f80c16a60300d524699b4a099d303b9f21fe
zallen <47335686+redallen@users.noreply.github.com>
2019-12-18 10:15:07 -0500

    bump repos for 2019.11 (#1591)
    * give up on 2019.09
    
    * new version

--- packages/patternfly-4/gatsby-node.js
+++ packages/patternfly-4/gatsby-node.js
@@ -1,5 +1,6 @@
-const nextConfig = require('./patternfly-next/gatsby-node');
-// Add types that are present in React in case we aren't building the React section
-exports.createSchemaCustomization = params => {
-  nextConfig.createSchemaCustomization(params);
+const { mdxTypeDefs } = require('gatsby-theme-patternfly-org');
+
+// https://www.gatsbyjs.org/docs/schema-customization/
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(mdxTypeDefs);
 }
---
+++ scripts/checkoutVersion.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra');
+const exec = require('child_process').execSync;
+const versions = require('gatsby-theme-patternfly-org/versions.json');
+const packageJsonOrg = require('patternfly-org-4/package.json');
+const packageJsonNext = require('../patternfly-4/patternfly-next/package.json');
+
+const versionToCheckout = process.argv[2] || '';
+const version = versions.Releases.find(({ name }) => name === versionToCheckout);
+
+
+if (!versionToCheckout || !version) {
+  console.error('Must specify valid version');
+  process.exit(1);
+}
+
+console.log('Checking out', versionToCheckout);
+
+function execWithLog(command) {
+  console.log('>', command);
+  console.log(exec(command).toString());
+}
+
+execWithLog('git pull --recurse-submodules');
+execWithLog(`cd packages/patternfly-4/patternfly-next && git merge v${version.versions['@patternfly/patternfly']}`);
+execWithLog(`cd packages/patternfly-4/patternfly-react && git merge @patternfly/react-core@${version.versions['@patternfly/react-core']}`);
+
+Object.entries(version.versions).forEach(([key, val]) => {
+  if (packageJsonOrg.dependencies[key]) {
+    packageJsonOrg.dependencies[key] = val;
+  }
+  if (packageJsonNext.dependencies[key]) {
+    packageJsonNext.dependencies[key] = val;
+  }
+});
+fs.writeFileSync(require.resolve('patternfly-org-4/package.json'), JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(require.resolve('../patternfly-4/patternfly-next/package.json'), JSON.stringify(packageJson, null, 2));
+
+execWithLog('yarn install');
+// TODO: git tag
