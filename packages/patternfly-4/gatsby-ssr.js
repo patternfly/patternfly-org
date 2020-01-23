a6fe4fecc188ab7445a7d5351539d46ff1b5f940
ddonahue007 <ddonahue@redhat.com>
2020-01-23 15:47:02 -0500

    feature(docs): Added Qualtrics survey to the site.

--- packages/patternfly-4/gatsby-ssr.js
+++ packages/patternfly-4/gatsby-ssr.js
@@ -1,7 +1,7 @@
 const React = require('react');
 // https://www.gatsbyjs.org/docs/ssr-apis
 
-exports.onRenderBody = ({ setHtmlAttributes, setPostBodyComponents }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setPostBodyComponents, setHeadComponents }) => {
   // Always en-us
   setHtmlAttributes({
     lang: 'en-us'
@@ -13,5 +13,9 @@ exports.onRenderBody = ({ setHtmlAttributes, setPostBodyComponents }) => {
   setPostBodyComponents([
     <script key="algogia-script" type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js" />,
     <link key="algogia-css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+  ]);
+  // QUALTRICS WEBSITE FEEDBACK SNIPPET
+  setHeadComponents([
+      <script type="text/javascript" src="qualtrics.js"/>
   ]);
 }
