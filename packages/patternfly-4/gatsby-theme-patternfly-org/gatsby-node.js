fa66ed209785e982ba157e0ddce95037b07a8aa2
ddonahue007 <ddonahue@redhat.com>
2020-01-27 15:16:02 -0500

    feature(docs): reverted changes that were no longer needed.

--- packages/patternfly-4/gatsby-theme-patternfly-org/gatsby-node.js
+++ packages/patternfly-4/gatsby-theme-patternfly-org/gatsby-node.js
@@ -182,7 +182,7 @@ exports.createPages = ({ actions, graphql }, pluginOptions) => graphql(`
     }
     const hbsInstance = createHandlebars(result.data.partials.nodes);
     const hiddenTitles = (pluginOptions.hiddenPages || []).map(title => title.toLowerCase());
-    const { showBanner = false, enableQualtrics = false } = pluginOptions;
+    const { showBanner = false } = pluginOptions;
 
     // Create 404 page
     actions.createPage({
@@ -225,9 +225,7 @@ exports.createPages = ({ actions, graphql }, pluginOptions) => graphql(`
             // To render static example HTML from patternfly-next
             htmlExamples: source === 'core' ? examples : undefined,
             // To hide the banner for core/React sites
-            showBanner,
-            // Run qualtrics JS snippet
-            enableQualtrics
+            showBanner
           }
         });
 
