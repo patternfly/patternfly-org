e1c425a8aa15b27239cea3394c58a0865897f2a3
redallen <zallen@redhat.com>
2020-03-19 15:13:38 -0400

    fix ci preview upload

--- .circleci/upload-preview.sh
+++ .circleci/upload-preview.sh
@@ -22,7 +22,7 @@ fi
 
 DEPLOY_DOMAIN_REACT="https://${DEPLOY_SUBDOMAIN}-react.surge.sh"
 DEPLOY_DOMAIN_ORG="https://${DEPLOY_SUBDOMAIN}-org.surge.sh"
-DEPLOY_DOMAIN_ORG_V3="https://${DEPLOY_SUBDOMAIN}-v3.surge.sh"
+DEPLOY_DOMAIN_ORG_V3="https://${DEPLOY_SUBDOMAIN}-org-v3.surge.sh"
 npx surge --project build/patternfly-react --domain ${DEPLOY_DOMAIN_REACT}
 npx surge --project build/patternfly-org --domain ${DEPLOY_DOMAIN_ORG}
 npx surge --project build/patternfly-org-3 --domain ${DEPLOY_DOMAIN_ORG_V3}
@@ -33,7 +33,7 @@ then
   # PR api requires comments be made on specific files of specific commits
   GITHUB_PR_COMMENTS="https://api.github.com/repos/${USERNAME}/${REPONAME}/issues/${PR_NUM}/comments"
   echo "Adding github PR comment ${GITHUB_PR_COMMENTS}"
-  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":React preview: '${DEPLOY_DOMAIN_REACT}'\nOrg preview: '${DEPLOY_DOMAIN_ORG}'\nOrg v3 preview: '${DEPLOY_DOMAIN_ORG_V3}'"}'
+  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":React preview: '${DEPLOY_DOMAIN_REACT}'\nOrg preview v3: '${DEPLOY_DOMAIN_ORG_V3}'/v3\nOrg preview: '${DEPLOY_DOMAIN_ORG}'"}'
 else
   echo "Already deployed ${DEPLOY_DOMAIN_REACT} and ${DEPLOY_DOMAIN_ORG}"
 fi
