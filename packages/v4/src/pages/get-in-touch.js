00822f75469a505b2868c82c10c17cbac84fe0e6
Evan <evan.wilkinson@gmail.com>
2020-07-20 10:24:59 -0400

    Fix/Add footer to homepage, 404, get in touch pages (#1972)
    * added footer to homepage
    
    * added footer to homepage, 404, and get in touch pages

--- packages/v4/src/pages/get-in-touch.js
+++ packages/v4/src/pages/get-in-touch.js
@@ -6,7 +6,7 @@ import "./get-in-touch.css";
 
 const GetInTouch = ({ location }) => {
   return (
-  <SideNavLayout location={location} hideSideNav showGdprBanner={true} pageTitle="Get in touch">
+  <SideNavLayout location={location} hideSideNav showGdprBanner={true} pageTitle="Get in touch" showFooter={true}>
     <PageSection
       variant={PageSectionVariants.light}
       className="ws-section ws-get-in-touch"
