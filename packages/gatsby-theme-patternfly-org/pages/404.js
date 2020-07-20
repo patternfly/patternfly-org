a2f9c81a8738d5b67d6d14e9ac2b4affd5cb21a5
evwilkin <evan.wilkinson@gmail.com>
2020-07-20 10:10:08 -0400

    added footer to homepage, 404, and get in touch pages

--- packages/gatsby-theme-patternfly-org/pages/404.js
+++ packages/gatsby-theme-patternfly-org/pages/404.js
@@ -5,7 +5,7 @@ import { SideNavLayout } from '../layouts/sideNavLayout/sideNavLayout';
 
 const Page404 = ({ location }) => {
   return (
-    <SideNavLayout location={location} hideSideNav showGdprBanner={true}>
+    <SideNavLayout location={location} hideSideNav showGdprBanner={true} showFooter={true}>
       <PageSection className="ws-section" style={{ height: '70vh' }}>
         <Title size="2xl" headingLevel="h1">404 - Page not found</Title>
         <p><Link to="/">Go home.</Link></p>
