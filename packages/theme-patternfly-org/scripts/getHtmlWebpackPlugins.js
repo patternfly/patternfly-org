b9406ba7da4ae4860e22a442bb0f5ea63effa217
redallen <zallen@redhat.com>
2020-07-23 10:49:52 -0400

    spacing

--- packages/theme-patternfly-org/scripts/getHtmlWebpackPlugins.js
+++ packages/theme-patternfly-org/scripts/getHtmlWebpackPlugins.js
@@ -8,7 +8,7 @@ module.exports = (routes, isProd) =>
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../template.ejs'),
       filename: `${url}/index.html`.replace(/^\/+/, ''),
-      title: `PatternFly 4${title ? `• ${title}` : ''}`,
+      title: `PatternFly 4${title ? ` • ${title}` : ''}`,
       templateParameters: {
         prerendering: isProd ? prerender(url) : 'Loading...',
         googleAnalyticsID: isProd ? googleAnalyticsID : false
