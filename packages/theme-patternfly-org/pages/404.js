6d073b780fd727f9d52719f0aa069186eb172a62
redallen <zallen@redhat.com>
2020-07-28 14:47:22 -0400

    default page exports

--- packages/theme-patternfly-org/pages/404.js
+++ packages/theme-patternfly-org/pages/404.js
@@ -3,15 +3,13 @@ import { Link } from 'theme-patternfly-org/components';
 import { PageSection, Title } from '@patternfly/react-core';
 import { SideNavLayout } from '../layouts/sideNavLayout/sideNavLayout';
 
-const Page404 = ({ location }) => {
-  return (
-    <SideNavLayout location={location} hideSideNav showGdprBanner={true} showFooter={true}>
-      <PageSection className="ws-section" style={{ height: '70vh' }}>
-        <Title size="2xl" headingLevel="h1">404 - Page not found</Title>
-        <p><Link to="/">Go home.</Link></p>
-      </PageSection>
-    </SideNavLayout>
-  );
-}
+const Page404 = ({ location }) => (
+  <SideNavLayout location={location} hideSideNav showGdprBanner={true} showFooter={true}>
+    <PageSection className="ws-section" style={{ height: '70vh' }}>
+      <Title size="2xl" headingLevel="h1">404 - Page not found</Title>
+      <p><Link to="/">Go home.</Link></p>
+    </PageSection>
+  </SideNavLayout>
+);
 
 export default Page404;
