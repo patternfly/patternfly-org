dcea2b76266b129754d3b2e4b1b465819624d220
redallen <zallen@redhat.com>
2020-08-10 16:21:55 -0400

    prerender source routes

--- packages/theme-patternfly-org/scripts/getHtmlWebpackPlugins.js
+++ packages/theme-patternfly-org/scripts/getHtmlWebpackPlugins.js
@@ -3,6 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { googleAnalyticsID, algolia } = require(`${process.cwd()}/patternfly-docs.config`);
 const { prerender } = require('./prerender');
 
+async function getHtmlWebpackPlugin(url, isProd, title) {
+  return new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, '../templates/html.ejs'),
+    filename: `${url}/index.html`.replace(/^\/+/, ''),
+    title: `PatternFly 4${title ? ` • ${title}` : ''}`,
+    templateParameters: {
+      prerendering: isProd ? await prerender(url) : 'Loading...',
+      // Don't use GA in dev mode
+      googleAnalyticsID: isProd ? googleAnalyticsID : false,
+      algolia
+    },
+    scriptLoading: 'defer',
+    inject: false
+  })
+}
+
 module.exports = async (routes, isProd) => {
   const res = [
     // Sitemap
@@ -17,23 +33,18 @@ module.exports = async (routes, isProd) => {
   ];
 
   const titledRoutes = Object.entries(routes)
+    .map(([url, { sources = [], ...props }]) => [
+      [url, props],
+      // Add pages for sources
+      ...(Object.values(sources).map(source => [source.slug, source]))
+    ])
+    .flat()
     .map(([url, { title, id }]) => [url, { title: title || id }]);
 
   for (const [url, { title }] of titledRoutes) {
-    res.push(new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../templates/html.ejs'),
-      filename: `${url}/index.html`.replace(/^\/+/, ''),
-      title: `PatternFly 4${title ? ` • ${title}` : ''}`,
-      templateParameters: {
-        prerendering: isProd ? await prerender(url) : 'Loading...',
-        // Don't use GA in dev mode
-        googleAnalyticsID: isProd ? googleAnalyticsID : false,
-        algolia
-      },
-      scriptLoading: 'defer',
-      inject: false
-    }));
+    res.push(await getHtmlWebpackPlugin(url, isProd, title));
   }
 
+  console.log('done prerendering')
   return res;
 };
