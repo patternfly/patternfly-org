9a128a8db44e3148517608bd3dab58213e3d93db
redallen <zallen@redhat.com>
2020-09-17 13:55:28 -0400

    copy static dir if exists

--- packages/theme-patternfly-org/scripts/webpack/webpack.client.config.js
+++ packages/theme-patternfly-org/scripts/webpack/webpack.client.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const { merge } = require('webpack-merge');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -15,6 +16,7 @@ catch {
   // Inside core workspace
   pfDir = path.join(process.cwd(), 'dist/assets');
 }
+const staticDir = path.join(process.cwd(), 'static/');
 // Don't include PatternFly styles twice
 const reactCSSRegex = /(react-[\w-]+\/dist|react-styles\/css)\/.*\.css$/;
 
@@ -72,22 +74,20 @@ const clientConfig = async (env, argv) => {
             {
               loader: 'css-loader'
             },
-            ...(!isProd ? [] : [
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                    plugins: [
-                      require('autoprefixer')({
-                        env: '>0.25%, not ie 11, not op_mini all',
-                        flexbox: false,
-                        grid: false
-                      })
-                    ]
-                  }
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    require('autoprefixer')({
+                      env: '>0.25%, not ie 11, not op_mini all',
+                      flexbox: false,
+                      grid: false
+                    })
+                  ]
                 }
               }
-            ])
+            }
           ]
         },
         {
@@ -106,7 +106,8 @@ const clientConfig = async (env, argv) => {
           // versions.json will later be copied to the root www dir
           { from: path.join(__dirname, '../../versions.json'), to: 'versions.json' },
           { from: path.join(pfDir, 'assets/images/'), to: 'assets/images/' },
-          { from: path.join(pfDir, 'assets/fonts/'), to: 'assets/fonts/' }
+          { from: path.join(pfDir, 'assets/fonts/'), to: 'assets/fonts/' },
+          ...(fs.existsSync(staticDir) ? [{ from: staticDir, to: '' }] : [])
         ]
       }),
       ...await getHtmlWebpackPlugins({ isProd, ...argv }), // Create an HTML page per route
