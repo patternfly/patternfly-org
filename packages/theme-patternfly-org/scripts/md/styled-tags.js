1e07b1beaf93336114413c8a0735680c8db14f18
zallen <47335686+redallen@users.noreply.github.com>
2020-10-20 13:55:41 -0400

    Fix/table breakpoints (#2170)
    * use react components (bad)
    
    * Revert "use react components (bad)"
    
    This reverts commit 138dcce898a96ea95e4922b23e93e88e2245bbe2.
    
    * transform tables to use pf-c-table
    
    * wrap in div

--- packages/theme-patternfly-org/scripts/md/styled-tags.js
+++ packages/theme-patternfly-org/scripts/md/styled-tags.js
@@ -24,6 +24,59 @@ function styledTags() {
         node.properties.className = node.properties.className || '';
         node.properties.className += node.properties.className ? ' ' : '';
         node.properties.className += `ws-${node.tagName}`;
+        // Match pf-c-table implementation
+        // https://pf4.patternfly.org/components/table/html/basic-table/
+        if (node.tagName === 'table') {
+          node.properties.className += ' pf-c-table pf-m-grid-lg';
+          node.properties.role = 'grid';
+          let columnHeaders = [];
+          for (let child of node.children) {
+            if (child.tagName === 'thead') {
+              // Find column headers
+              const tr = child.children.find(child => child.tagName === 'tr');
+              tr.properties.role = 'row';
+              tr.children
+                .filter(child => child.tagName === 'th')
+                .forEach(th => {
+                  th.properties.role = 'columnheader';
+                  th.properties.scope = 'col';
+                  let colName = '';
+                  visit(th, 'text', node2 => colName += node2.value);
+                  columnHeaders.push(colName);
+                  // Need to wrap in div for css-grid stuffs
+                  th.children = [{
+                    type: 'element',
+                    tagName: 'div',
+                    properties: {},
+                    children: th.children
+                  }];
+                });
+            }
+            else if (child.tagName === 'tbody') {
+              child.properties.role = 'rowgroup';
+              child.children
+                .filter(tr => tr.tagName === 'tr')
+                .forEach(tr => {
+                  tr.properties.role = 'row';
+                  tr.children
+                    .filter(td => td.tagName === 'td')
+                    .forEach((td, i) => {
+                      td.properties.role = 'cell';
+                      if (columnHeaders[i]) {
+                        td.properties['data-label'] = columnHeaders[i];
+                      }
+                      // Need to wrap in div for css-grid stuffs
+                      td.children = [{
+                        type: 'element',
+                        tagName: 'div',
+                        properties: {},
+                        children: td.children
+                      }];
+                    });
+                });
+            }
+          }
+        }
       }
     });
   }
