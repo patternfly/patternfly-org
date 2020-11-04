6f7928f93968f5371739760d9dff6448fca43e08
evwilkin <evan.wilkinson@gmail.com>
2020-11-04 13:09:28 -0500

    PR feedback & add Twitter

--- packages/v4/src/pages/community.js
+++ packages/v4/src/pages/community.js
@@ -10,6 +10,7 @@ import ChatIcon from '@patternfly/react-icons/dist/esm/icons/chat-icon';
 import QuestionIcon from '@patternfly/react-icons/dist/esm/icons/question-icon';
 import CatalogIcon from '@patternfly/react-icons/dist/esm/icons/catalog-icon';
 import MailBulkIcon from '@patternfly/react-icons/dist/esm/icons/mail-bulk-icon';
+import TwitterIcon from '@patternfly/react-icons/dist/esm/icons/twitter-icon';
 
 const CommunityPage = () => (
   <PageSection variant={PageSectionVariants.light}>
@@ -72,18 +73,8 @@ const CommunityPage = () => (
       <br/>
       <p><a href="https://patternfly.github.io/patternfly-elements/"><strong>PatternFly Elements:</strong></a> PatternFly Elements is a community created by Red Hat's web-based developers. It focuses on creating web components for use across Red Hat's sites and SaaS products and features theming options available for your own brand library.</p>
 
-      <Title size="4xl" className="pf-u-mb-lg ws-page-title pf-u-text-align-center ws-keep-growing pf-u-mt-3xl" headingLevel="h2">Let's keep growing</Title>
-      <p className="pf-u-text-align-center">The PatternFly community is never finished growing, and we want to keep it that way. Feel free to contribute whenever — we’re always open.</p>
-
-      <p className="pf-u-text-align-center ws-get-in-touch">
-        <Link to="#get-in-touch"><strong>Get in touch</strong></Link>
-      </p> 
-
-      <Title size="4xl" className="pf-u-mb-lg ws-page-title" headingLevel="h2" id="get-in-touch">Get in touch</Title>
-
-      <p>Get involved with the PatternFly community. Get answers to your questions and share your knowledge with others.</p>
-
-      <p>We are here to help.</p>
+      <Title size="4xl" className="pf-u-mb-lg ws-page-title ws-keep-growing pf-u-mt-3xl" headingLevel="h2">Let's keep growing</Title>
+      <p>The PatternFly community is never finished growing, and we want to keep it that way. Feel free to reach out whenever — we’re always open.</p>
 
       <Grid sm={12} md={6} gutter="sm" className="pf-u-my-lg pf-l-grid pf-m-all-12-col-on-sm pf-m-all-6-col-on-md pf-m-gutter" style={{ maxWidth: '450px' }}>
         <GridItem>
@@ -119,6 +110,15 @@ const CommunityPage = () => (
             <SplitItem isFilled>
               <Title size="lg" className="ws-title" headingLevel="h4">Read the latest</Title>
               <a href="//medium.com/patternfly" target="_blank" rel="noopener noreferrer">PatternFly Medium</a>
+            </SplitItem>
+          </Split>
+        </GridItem>
+        <GridItem>
+          <Split>
+            <SplitItem style={{ marginRight: '12px' }}><TwitterIcon /></SplitItem>
+            <SplitItem isFilled>
+              <Title size="lg" className="ws-title" headingLevel="h4">Send us a Tweet</Title>
+              <a href="//twitter.com/patternfly" target="_blank" rel="noopener noreferrer">PatternFly Twitter</a>
             </SplitItem>
           </Split>
         </GridItem>
