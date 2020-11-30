18ccd38a7916bfd2afc0d20b3669c658505e4c0a
zallen <47335686+redallen@users.noreply.github.com>
2020-11-30 14:36:31 -0500

    fix JSXText replace regex (#2295)
    * fix JSXText replace regex
    
    * tweak regex

--- packages/theme-patternfly-org/helpers/buble.js
+++ packages/theme-patternfly-org/helpers/buble.js
@@ -58,7 +58,7 @@ const generator = Object.assign({}, baseGenerator, {
     }
     if (node.value) {
       state.write(',"');
-      state.write(node.value.replace(/"/g, '\\"'));
+      state.write(node.value.replace(/"/g, '\\"').replace(/^\s+/, '').replace(/\s*\n\s*/g, ''));
       state.write('"');
     }
   },
