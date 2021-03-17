ab4f6defc873ae6e2536103f8bcd5f7996041ec9
zallen <47335686+redallen@users.noreply.github.com>
2021-03-17 14:52:37 -0400

    fix(buble): perfect JSXText regex? (#2413)

--- packages/theme-patternfly-org/helpers/buble.js
+++ packages/theme-patternfly-org/helpers/buble.js
@@ -58,7 +58,7 @@ const generator = Object.assign({}, baseGenerator, {
     }
     if (node.value) {
       state.write(',"');
-      state.write(node.value.replace(/"/g, '\\"').replace(/^\s+/, '').replace(/\s*\n\s*/g, ''));
+      state.write(node.value.replace(/"/g, '\\"').replace(/\n\s*/g, ''));
       state.write('"');
     }
   },
@@ -111,3 +111,4 @@ function transform(code) {
 module.exports = {
   transform
 };
+
