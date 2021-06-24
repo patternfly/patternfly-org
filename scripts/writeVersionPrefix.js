f550a53f68437dc4084a07ffc3f6d0bc7fe36f00
redallen <zallen@redhat.com>
2021-06-24 09:42:01 -0400

    fix copying non-utf8 files

--- scripts/writeVersionPrefix.js
+++ scripts/writeVersionPrefix.js
@@ -10,13 +10,15 @@ glob.sync(path.join(__dirname, '../build/patternfly-org/v4/**'))
   .forEach(file => {
     const newPath = file.replace('patternfly-org/v4', `patternfly-org/${version}`);
 
-    let contents = fs.readFileSync(file, 'utf8');
     if (file.endsWith('.js') || file.endsWith('.html')) {
+      let contents = fs.readFileSync(file, 'utf8');
       contents = contents.replace(/\/v4/g, `/${version}`);
+      fs.outputFileSync(newPath, contents);
+    } else {
+      fs.ensureDirSync(path.dirname(newPath));
+      fs.copyFileSync(file, newPath);
     }
 
-    fs.ensureFileSync(newPath);
-    fs.writeFileSync(newPath, contents);
   });
 
 fs.writeFileSync(
