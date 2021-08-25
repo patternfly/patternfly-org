a930a84816e7b1a82578a4b6fbad8e00746b5720
zallen <47335686+redallen@users.noreply.github.com>
2021-08-25 15:17:52 -0400

    improve cli load times (#2657)

--- packages/theme-patternfly-org/scripts/cli/cli.js
+++ packages/theme-patternfly-org/scripts/cli/cli.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
-const { start } = require('./start');
-const { generate } = require('./generate');
-const { build } = require('./build');
-const { serve } = require('./serve');
-const { version } = require('../../package.json');
 
 program
-  .version(version)
   .option('-c, --config <path>', 'set config path', './patternfly-docs.config.js')
   .option('-css, --cssconfig <path>', 'set css import file path', './patternfly-docs.css.js')
   .option('-s, --source <path>', 'set source generation file path', './patternfly-docs.source.js');
@@ -15,24 +9,36 @@ program
 program
   .command('generate')
   .description('generates source files')
-  .action(generate);
+  .action(options => {
+    const { generate } = require('./generate');
+    generate(options);
+  });
 
 program
   .command('start')
   .description('generates source files and runs webpack-dev-server')
-  .action(start);
+  .action(options => {
+    const { start } = require('./start');
+    start(options);
+  });
 
 program
   .command('build <server|client|all>')
   .option('-a, --analyze', 'use webpack-bundle-analyzer', false)
   .description('generates source files and runs webpack')
-  .action(build);
+  .action((cmd, options) => {
+    const { build } = require('./build');
+    build(cmd, options);
+  });
 
 program
   .command('serve <folder>')
   .option('-p, --port <port>', 'port to serve on', 5000)
   .description('serves a directory on a port')
-  .action(serve);
+  .action((cmd, options) => {
+    const { serve } = require('./serve');
+    serve(cmd, options);
+  });
 
 program
   .command('screenshots')
