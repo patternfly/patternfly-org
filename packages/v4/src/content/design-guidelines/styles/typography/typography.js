fccdfd8201355e3b32cf0940faff97b7901e1eca
Matt Carrano <mcarrano@redhat.com>
2022-02-15 16:49:27 -0500

    Declare 'textStyle'

--- packages/v4/src/content/design-guidelines/styles/typography/typography.js
+++ packages/v4/src/content/design-guidelines/styles/typography/typography.js
@@ -113,7 +113,7 @@ export const styleProps = {
   }
 }
 
-export const TypographyGrid = ({title, note, symbol, fontWeight, fontWeightText, fontSize, variableName, lineHeight, fontFamily, className}) => (
+export const TypographyGrid = ({title, note, symbol, fontWeight, fontWeightText, fontSize, variableName, lineHeight, fontFamily, textStyle, className}) => (
   <React.Fragment>
     <h3 className={className ? `ws-title ${className}` : 'ws-title'}>{title} {symbol && <span className="ws-typography-gridTitleSymbol">{symbol}</span>}</h3>
     <PfStyledText fontFamily={fontFamily} fontWeight={fontWeight} variableName={variableName} lineHeight={lineHeight}>
