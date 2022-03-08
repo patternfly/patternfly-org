0aa885661b7acd8795af628aa15d79de5f8526ef
evwilkin <evan.wilkinson@gmail.com>
2022-03-08 11:21:40 -0500

    dynamic jumplinks offset based on screen size

--- packages/theme-patternfly-org/components/tableOfContents/tableOfContents.js
+++ packages/theme-patternfly-org/components/tableOfContents/tableOfContents.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Title, JumpLinks, JumpLinksItem, JumpLinksList, ExpandableSection } from '@patternfly/react-core';
+import { JumpLinks, JumpLinksItem, JumpLinksList } from '@patternfly/react-core';
 import './tableOfContents.css';
 import { trackEvent } from '../../helpers';
 
 export const TableOfContents = ({ items }) => {
-  function renderItem(item, index) {
+  const [width, setWidth] = React.useState(window.innerWidth);
+
+  const updateWidth = () => {
+    const { innerWidth } = window;
+    innerWidth !== width && setWidth(innerWidth);
+  }
+
+  const renderItem = (item, index) => {
     return Array.isArray(item)
       ? (
         <JumpLinksList key={index} className="ws-toc-sublist">
           {item.map(renderItem)}
         </JumpLinksList>
       ) : (
-        <JumpLinksItem key={item.id} href={`#${item.id}`} className="ws-toc-item" onClick={() => trackEvent('jump_link_click', 'click_event', item.id.toUpperCase())
+        <JumpLinksItem
+          key={item.id}
+          href={`#${item.id}`}
+          className="ws-toc-item"
+          onKeyDown={updateWidth}
+          onMouseDown={updateWidth}
+          onClick={() => trackEvent('jump_link_click', 'click_event', item.id.toUpperCase())
         }>
           {item.text}
         </JumpLinksItem>
@@ -24,7 +37,7 @@ export const TableOfContents = ({ items }) => {
       isVertical
       scrollableSelector="#ws-page-main"
       className="ws-toc"
-      offset={148}
+      offset={width > 1450 ? 92 : 148}
       expandable={{ default: 'expandable', '2xl': 'nonExpandable' }}
     >
       {items.map(renderItem)}
