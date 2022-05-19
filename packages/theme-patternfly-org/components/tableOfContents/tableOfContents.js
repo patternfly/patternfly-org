c7674cb6905389f22f6a7438dd118a10eb17ceec
evwilkin <evan.wilkinson@gmail.com>
2022-05-19 12:13:44 -0400

    fix(toc): fixed jumplinks in TOC to resolve a11y nesting issue

--- packages/theme-patternfly-org/components/tableOfContents/tableOfContents.js
+++ packages/theme-patternfly-org/components/tableOfContents/tableOfContents.js
@@ -10,25 +10,57 @@ export const TableOfContents = ({ items }) => {
     const { innerWidth } = window;
     innerWidth !== width && setWidth(innerWidth);
   }
+  let wasSublistRendered = false;
 
-  const renderItem = (item, index) => {
-    return Array.isArray(item)
-      ? (
-        <JumpLinksList key={index} className="ws-toc-sublist">
-          {item.map(renderItem)}
+  const renderSublist = (item, nextItem, idx) => {
+    wasSublistRendered = true;
+    return (
+      <>
+        {item.text}
+        <JumpLinksList>
+          {nextItem.map(curItem => (
+            <JumpLinksItem
+              key={curItem.id}
+              href={`#${curItem.id}`}
+              className="ws-toc-item"
+              onKeyDown={updateWidth}
+              onMouseDown={updateWidth}
+              onClick={() => trackEvent('jump_link_click', 'click_event', curItem.id.toUpperCase())}
+            >
+              {curItem.text}
+            </JumpLinksItem>
+          ))}
         </JumpLinksList>
-      ) : (
+      </>
+    );
+  }
+
+  let jumpLinksItems = [];
+
+  const renderJumpLinksItems = () => {
+    items.forEach((item, index) => {
+      let cur = item;
+      let next = items[index + 1];
+      // Don't render empty <JumpLinksItem> for an array of sublist items
+      if (wasSublistRendered) {
+        wasSublistRendered = false;
+        return null;
+      }
+
+      jumpLinksItems.push(
         <JumpLinksItem
           key={item.id}
           href={`#${item.id}`}
           className="ws-toc-item"
           onKeyDown={updateWidth}
           onMouseDown={updateWidth}
-          onClick={() => trackEvent('jump_link_click', 'click_event', item.id.toUpperCase())
-        }>
-          {item.text}
+          onClick={() => trackEvent('jump_link_click', 'click_event', item.id.toUpperCase())}
+        >
+          { Array.isArray(next) ? renderSublist(item, next, index) : item.text }
         </JumpLinksItem>
-      )
+      );
+    })
+    return jumpLinksItems;
   }
 
   return (
@@ -40,7 +72,7 @@ export const TableOfContents = ({ items }) => {
       offset={width > 1450 ? 92 : 148}
       expandable={{ default: 'expandable', '2xl': 'nonExpandable' }}
     >
-      {items.map(renderItem)}
+      { renderJumpLinksItems() }
     </JumpLinks>
   );
 }
