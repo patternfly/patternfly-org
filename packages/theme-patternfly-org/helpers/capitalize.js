9c7c7f16edee7bf61beb5834d98d539332b44663
Dana Gutride <dgutride@gmail.com>
2022-07-06 08:45:24 -0400

    chore: Remove v4 directories, cleanup package.json and add readme info

--- packages/theme-patternfly-org/helpers/capitalize.js
+++
@@ -1,7 +0,0 @@
-function capitalize(input) {
-  return input[0].toUpperCase() + input.substring(1);
-}
-
-module.exports = {
-  capitalize
-};
--- packages/theme-patternfly-org/helpers/getId.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  getId: text => text
-    .toLowerCase()
-    .replace(/ /g, '-')
-    .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g, '')
-}
