e8283ab8aa86a4a9afb701459ce668ec91b2ed05
Evan <evan.wilkinson@redhat.com>
2023-01-03 16:43:49 -0500

    fix bug passing index in map function

--- packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
+++ packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
@@ -47,7 +47,9 @@ const HeaderTools = ({
   const [searchValue, setSearchValue] = React.useState('');
   const [isSearchExpanded, setIsSearchExpanded] = React.useState(false);
 
-  const getDropdownItem = (version, isLatest = false) => (
+  const getDropdownItem = (version, isLatest = false) => {
+    console.log({version, isLatest});
+    return (
     <DropdownItem
       key={version.name}
       component={
@@ -56,7 +58,8 @@ const HeaderTools = ({
         </a>
       }
     />
-  );
+    );
+  }
 
   const onChange = (value) => {
     setSearchValue(value);
@@ -135,7 +138,7 @@ const HeaderTools = ({
                     {Object.values(versions.Releases)
                       .filter(version => !version.hidden && !version.latest)
                       .slice(0,3)
-                      .map(getDropdownItem)}
+                      .map(version => getDropdownItem(version))}
                   </DropdownGroup>,
                   <Divider key="divider" className="ws-switcher-divider"/>,
                   <DropdownItem
