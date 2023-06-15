9738a148d274d09ad994385160d3908ba18ecfdb
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2023-06-15 16:39:51 -0400

    Update v5 links, references, redirects (#3552)

--- scripts/writeVersionPrefix.js
+++ scripts/writeVersionPrefix.js
@@ -5,14 +5,14 @@ const versions = require('@patternfly/documentation-framework/versions.json');
 
 const version = versions.Releases.find(release => release.latest).name;
 
-glob.sync(path.join(__dirname, '../build/patternfly-org/v4/**'))
+glob.sync(path.join(__dirname, "../build/patternfly-org/site/**"))
   .filter(file => fs.lstatSync(file).isFile())
   .forEach(file => {
-    const newPath = file.replace('patternfly-org/v4', `patternfly-org/${version}`);
+    const newPath = file.replace("patternfly-org/site", `patternfly-org/${version}`);
 
     if (file.endsWith('.js') || file.endsWith('.html')) {
       let contents = fs.readFileSync(file, 'utf8');
-      contents = contents.replace(/\/v4/g, `/${version}`);
+      contents = contents.replace(/\/site/g, `/${version}`);
       fs.outputFileSync(newPath, contents);
     } else {
       fs.ensureDirSync(path.dirname(newPath));
