fdfba3e79c74fd4d816a63f3a38de9a024c82766
Katie McFaul <kmcfaul@redhat.com>
2024-04-23 14:49:42 -0400

    feat(docs): fix for missing props tables

--- packages/documentation-framework/templates/patternfly-docs/patternfly-docs.source.js
+++ packages/documentation-framework/templates/patternfly-docs/patternfly-docs.source.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = (sourceMD, sourceProps) => {
   /** Parse source content for props so that we can display them. You must source props before sourcing the markdown
   files, otherwise the props table won't be rendered.
   */
-  const propsIgnore = ['**/*.test.tsx', '**/examples/*.tsx'];
-  const extensionPath = path.join(__dirname, '../src');
-  sourceProps(path.join(extensionPath, '/**/*.tsx'), propsIgnore);
+  const propsIgnore = ["/**/*.test.tsx", "/**/examples/*.tsx"];
+  const extensionPath = path.join(__dirname, "../src");
+  sourceProps(path.join(extensionPath, "/**/*.tsx"), propsIgnore);
 
-  // Parse md files 
-  const contentBase = path.join(__dirname, './content');
-  sourceMD(path.join(contentBase, 'extensions/**/*.md'), 'extensions');
+  // Parse md files
+  const contentBase = path.join(__dirname, "./content");
+  sourceMD(path.join(contentBase, "extensions/**/*.md"), "extensions");
 
   /**
     If you want to parse content from node_modules instead of providing a relative/absolute path, 
@@ -23,4 +23,4 @@ module.exports = (sourceMD, sourceProps) => {
     sourceMD(path.join(extensionPath, '../patternfly-docs/**\/demos/*.md'), 'react-demos');
     sourceMD(path.join(extensionPath, '../patternfly-docs/**\/design-guidelines/*.md'), 'design-guidelines');
   */
-}
+};
