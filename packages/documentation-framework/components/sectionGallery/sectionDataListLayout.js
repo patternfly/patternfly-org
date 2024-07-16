e57f28d3fbbe02c1b29135d25fff1f7ea57e448b
Evan <evan.wilkinson@redhat.com>
2024-07-16 13:43:21 -0400

    chore(docs): sectiongallery - remove unused styles, cleanup code (#4140)

--- packages/documentation-framework/components/sectionGallery/sectionDataListLayout.js
+++ packages/documentation-framework/components/sectionGallery/sectionDataListLayout.js
@@ -1,51 +1,98 @@
-import React from "react";
-import { DataList, DataListItem, DataListItemRow, DataListItemCells, DataListCell, Split, SplitItem, TextContent, Text, TextVariants, Label } from "@patternfly/react-core";
+import React from 'react';
+import {
+  DataList,
+  DataListItem,
+  DataListItemRow,
+  DataListItemCells,
+  DataListCell,
+  Split,
+  SplitItem,
+  TextContent,
+  Text,
+  TextVariants,
+  Label,
+} from '@patternfly/react-core';
 import { Link } from '../link/link';
 import { TextSummary } from './TextSummary';
 
-export const SectionDataListLayout = ({ galleryItems, layoutView, hasListText, hasListImages }) => {
+export const SectionDataListLayout = ({
+  galleryItems,
+  layoutView,
+  hasListText,
+  hasListImages,
+}) => {
   if (layoutView !== 'list') {
     return null;
   }
 
   return (
     <DataList onSelectDataListItem={() => {}}>
-      {galleryItems.map(({ idx, slug, illustration, itemName, title, isBeta, isDeprecated, isDemo, id, galleryItemsData }) => (
-        <Link to={slug} key={idx} className="ws-section-gallery-item">
-          <DataListItem>
-            <DataListItemRow>
-              <DataListItemCells dataListCells={[
-                hasListImages && illustration && (
-                  <DataListCell width={1} key="illustration">
-                    <div>
-                      <img src={illustration} alt={`${itemName} illustration`} />
-                    </div>
-                  </DataListCell>
-                ),
-                <DataListCell width={5} key="text-description">
-                  <Split className={ hasListText ? "pf-v6-u-mb-md" : null }>
-                    <SplitItem isFilled>
-                      <TextContent>
-                        <Text component={TextVariants.h2}>
-                          <span>
-                            {title}
-                          </span>
-                        </Text>
-                      </TextContent>
-                    </SplitItem>
-                    <SplitItem>
-                      {isBeta && (<Label color="blue" isCompact>Beta</Label>)}
-                      {!isBeta && isDeprecated && (<Label color="grey" isCompact>Deprecated</Label>)}
-                      {!isBeta && !isDeprecated && isDemo && (<Label color="purple" isCompact>Demo</Label>)}
-                    </SplitItem>
-                  </Split>
-                  { hasListText && <TextSummary id={id} itemsData={galleryItemsData} /> }
-                </DataListCell>
-              ]} />
-            </DataListItemRow>
-          </DataListItem>
-        </Link>
-      ))}
+      {galleryItems.map(
+        ({
+          idx,
+          slug,
+          illustration,
+          itemName,
+          title,
+          isBeta,
+          isDeprecated,
+          isDemo,
+          id,
+          galleryItemsData,
+        }) => (
+          <Link to={slug} key={idx} className="ws-section-gallery-item">
+            <DataListItem>
+              <DataListItemRow>
+                <DataListItemCells
+                  dataListCells={[
+                    hasListImages && illustration && (
+                      <DataListCell width={1} key="illustration">
+                        <div>
+                          <img
+                            src={illustration}
+                            alt={`${itemName} illustration`}
+                          />
+                        </div>
+                      </DataListCell>
+                    ),
+                    <DataListCell width={5} key="text-description">
+                      <Split className={hasListText ? 'pf-v6-u-mb-md' : null}>
+                        <SplitItem isFilled>
+                          <TextContent>
+                            <Text component={TextVariants.h2}>
+                              <span>{title}</span>
+                            </Text>
+                          </TextContent>
+                        </SplitItem>
+                        <SplitItem>
+                          {isBeta && (
+                            <Label color="blue" isCompact>
+                              Beta
+                            </Label>
+                          )}
+                          {!isBeta && isDeprecated && (
+                            <Label color="grey" isCompact>
+                              Deprecated
+                            </Label>
+                          )}
+                          {!isBeta && !isDeprecated && isDemo && (
+                            <Label color="purple" isCompact>
+                              Demo
+                            </Label>
+                          )}
+                        </SplitItem>
+                      </Split>
+                      {hasListText && (
+                        <TextSummary id={id} itemsData={galleryItemsData} />
+                      )}
+                    </DataListCell>,
+                  ]}
+                />
+              </DataListItemRow>
+            </DataListItem>
+          </Link>
+        )
+      )}
     </DataList>
-  )
+  );
 };
