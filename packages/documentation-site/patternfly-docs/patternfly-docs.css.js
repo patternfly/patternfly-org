c49eb3a1aff14fa0a6af5e3abeb3d49f91d186c3
Katie McFaul <kmcfaul@redhat.com>
2024-08-08 17:16:08 -0400

    fix log-viewer styles

--- packages/documentation-site/patternfly-docs/patternfly-docs.css.js
+++ packages/documentation-site/patternfly-docs/patternfly-docs.css.js
@@ -1,17 +1,19 @@
 // React-specific CSS
-import '@patternfly/react-styles/src/css/components/Table/inline-edit.css'; // inline-edit extension styles
-import '@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css';
+import "@patternfly/react-styles/src/css/components/Table/inline-edit.css"; // inline-edit extension styles
+import "@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css";
 // Patternfly
-import '@patternfly/patternfly/patternfly.css';
+import "@patternfly/patternfly/patternfly.css";
 // Patternfly utilities
-import '@patternfly/patternfly/patternfly-addons.css';
+import "@patternfly/patternfly/patternfly-addons.css";
 // Global theme CSS
-import '@patternfly/documentation-framework/global.css';
+import "@patternfly/documentation-framework/global.css";
 // Patternfly topology
-import '@patternfly/react-topology/src/css/topology-controlbar.css';
-import '@patternfly/react-topology/src/css/topology-side-bar.css';
-import '@patternfly/react-topology/src/css/topology-view.css';
-import '@patternfly/react-topology/src/css/topology-components.css';
-import '@patternfly/react-topology/src/css/topology-pipelines.css';
-// PatternFly log viewer
-// import '@patternfly/react-log-viewer/dist/css/log-viewer.css';
+import "@patternfly/react-topology/src/css/topology-controlbar.css";
+import "@patternfly/react-topology/src/css/topology-side-bar.css";
+import "@patternfly/react-topology/src/css/topology-view.css";
+import "@patternfly/react-topology/src/css/topology-components.css";
+import "@patternfly/react-topology/src/css/topology-pipelines.css";
+// Patternfly log viewer
+import "@patternfly/react-log-viewer/src/LogViewer/css/log-viewer.css";
+// Patternfly user feedback
+// import "@patternfly/react-user-feedback/src/Feedback/Feedback.css"; waiting for user-feedback update
