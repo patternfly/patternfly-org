f652d190a8b8640e9d5be6e95918ff74f6deb556
Evan <evan.wilkinson@gmail.com>
2024-08-22 15:53:37 -0400

    Discard changes to packages/documentation-framework/components/gdprBanner/gdprBanner.js

--- packages/documentation-framework/components/gdprBanner/gdprBanner.js
+++ packages/documentation-framework/components/gdprBanner/gdprBanner.js
@@ -1,58 +1,37 @@
 import React from 'react';
-import {
-  Panel,
-  PanelMain,
-  PanelMainBody,
-  Flex,
-  FlexItem,
-  Button,
-} from '@patternfly/react-core';
+import { Panel, PanelMain, PanelMainBody, Flex, FlexItem, Button } from '@patternfly/react-core';
 import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
 import './gdprBanner.css';
 
 export const GdprBanner = () => {
-  const initialBannerOpen =
-    typeof window !== 'undefined' &&
-    window.localStorage &&
-    !localStorage.getItem('gdpr-banner-closed');
-  const [isBannerOpen, setBannerOpen] = React.useState(initialBannerOpen);
+  const initialBannerOpen = typeof window !== 'undefined' && window.localStorage && !localStorage.getItem('gdpr-banner-closed');
+  const [ isBannerOpen, setBannerOpen ] = React.useState(initialBannerOpen);
 
   const closeBanner = () => {
     localStorage.setItem('gdpr-banner-closed', 'true');
     setBannerOpen(false);
-  };
+  }
 
   return !isBannerOpen ? null : (
     <Flex className="ws-gdpr-banner-container">
-      <Panel variant="bordered" className="pf-m-raised" id="ws-gdpr-banner">
-        <PanelMain>
-          <PanelMainBody>
-            <Flex
-              flexWrap={{ default: 'nowrap' }}
-              alignItems={{ default: 'alignItemsFlexStart' }}
-            >
-              <FlexItem className="pf-v6-u-ml-xl">
-                We use cookies on our websites to deliver our online services.
-                Details about how we use cookies and how you may disable them
-                are set out in our{' '}
-                <a href="//www.redhat.com/en/about/privacy-policy">
-                  Privacy Statement
-                </a>
-                . By using this website you agree to our use of cookies.
-              </FlexItem>
-              <FlexItem>
-                <Button
-                  variant="plain"
-                  aria-label="Close banner"
-                  onClick={closeBanner}
-                >
-                  <TimesIcon />
-                </Button>
-              </FlexItem>
-            </Flex>
-          </PanelMainBody>
-        </PanelMain>
-      </Panel>
+    <Panel variant="bordered" className="pf-m-raised" id="ws-gdpr-banner">
+      <PanelMain>
+        <PanelMainBody>
+          <Flex flexWrap={{default: 'nowrap'}} alignItems={{default: 'alignItemsFlexStart'}}>
+            <FlexItem className="pf-v6-u-ml-xl">
+              We use cookies on our websites to deliver our online services.
+              Details about how we use cookies and how you may disable them are set out in our <a href="//www.redhat.com/en/about/privacy-policy">Privacy Statement</a>.
+              By using this website you agree to our use of cookies.
+            </FlexItem>
+            <FlexItem>
+              <Button variant="plain" aria-label="Close banner" onClick={closeBanner}>
+                <TimesIcon />
+              </Button>
+            </FlexItem>
+          </Flex>
+        </PanelMainBody>
+      </PanelMain>
+    </Panel>
     </Flex>
   );
-};
+}
