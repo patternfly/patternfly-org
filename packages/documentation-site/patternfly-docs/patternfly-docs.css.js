ccab899478cb48eafd0f39eca998f790d6ffb38b
Donald Labaj <dlabaj@redhat.com>
2024-10-30 19:47:58 -0400

    fix: Updated with patch from virtual assistant. (#4358)
    * fix: Updated with patch from virtual assistant.
    
    * fix: moved main.css to bottom of css imports.

--- packages/documentation-site/patternfly-docs/patternfly-docs.css.js
+++ packages/documentation-site/patternfly-docs/patternfly-docs.css.js
@@ -1,6 +1,7 @@
 // React-specific CSS
 import "@patternfly/react-styles/src/css/components/Table/inline-edit.css"; // inline-edit extension styles
 import "@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css";
+
 // Patternfly
 import "@patternfly/patternfly/patternfly.css";
 // Patternfly utilities
