91d222a7cc2a4e7c9d073badba40123635d2e612
nicolethoen <nthoen@redhat.com>
2024-11-25 15:36:54 -0500

    revert a couple things

--- packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
+++ packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
@@ -249,7 +249,7 @@ function isDarkModeEnabled() {
   const storedValue = localStorage.getItem(DARK_MODE_STORAGE_KEY);
   const isEnabled = storedValue === null ? mediaQuery.matches : storedValue === "true";
 
-  return false;
+  return isEnabled;
 }
 
 export const SideNavLayout = ({ children, groupedRoutes, navOpen: navOpenProp }) => {
--- packages/documentation-framework/scripts/cli/cli.js
+++ packages/documentation-framework/scripts/cli/cli.js
@@ -47,7 +47,6 @@ program
   .option('-u, --urlPrefix <prefix>', 'where fullscreen pages are hosted', 'http://localhost:5000')
   .option('-a, --allRoutes', 'true if screenshots of all examples - not just full screen', false)
   .option('-f --filterTerm <term>', 'search term to filter routes by', '')
-  .option('-d, --darkTheme', 'true if dark theme should be on in screenshots', false)
   .description('updates screenshots for generated components')
   .action(options => {
     const { writeScreenshots } = require('../writeScreenshots');
