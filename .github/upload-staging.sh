9705f284de4abd1eb32494153ebd0515cc114a37
Donald Labaj <donald.labaj@gmail.com>
2025-05-19 23:13:59 -0400

    chore: Setup site folder and doc core. chore: Added generated content typescript node script.
    chore: Fixed build issue with generating files.
    
    chore: updated to remove dist folder.
    
    chore: updated gitignore.
    
    package.json updates.
    
    fix: Added CI/CD code.
    fix: removed _template.md
    
    chore: Updated to remove generate content.
    
    chore: updated to fix bad import.
    
    chore: clean up.
    
    chore: clean up.
    
    chore: Added about patternfly.
    
    chore: Updates to get started
    
    chore: updated lerna.json
    chore: Updated upload-staging.sh

--- .github/upload-staging.sh
+++ .github/upload-staging.sh
@@ -18,6 +18,19 @@ aws cloudfront create-invalidation --distribution-id EQE4NPRH0CQXJ --paths "/*"
 
 if [ -d "packages/site/dist" ]; then
   aws s3 rm --recursive s3://patternfly-org-preview
+  aws s3 sync packages/site/dist s3://patternfly-org-preview --exclude "*" \
+    --include "*.html" \
+    --include "*.json" \
+    --exclude "static/**/*.json" \
+    --include "sw.js" \
+    --cache-control "public, max-age=0, must-revalidate"
+
   aws s3 sync packages/site/dist s3://patternfly-org-preview --include "*" \
+    --exclude "*.html" \
+    --exclude "*.json" \
+    --include "static/**/*.json" \
+    --exclude "sw.js" \
     --cache-control "public, max-age=31536000, immutable"
+
+  aws cloudfront create-invalidation --distribution-id E2IXSH9IWFQCUC --paths "/*"
 fi
