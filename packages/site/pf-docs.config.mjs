f5cada48e8ca2f02cc09f2bdaeeedb8b578afdf6
Donald Labaj <donald.labaj@gmail.com>
2025-06-04 15:29:01 -0400

    fix: Added CI/CD code.

--- packages/site/pf-docs.config.mjs
+++ packages/site/pf-docs.config.mjs
@@ -2,11 +2,11 @@ export const config = {
   content: [
     // example content entry for local content, this would feed all markdown files in the content directory to the
     // documentation core with a content identifier of 'content':
-    // {
-    //   base: 'content',
-    //   pattern: "*.md",
-    //   name: 'content'
-    // },
+    {
+      base: 'src/generated/content',
+      pattern: "*.md",
+      name: 'content'
+    },
     //
     // example content entry for remote content, this would fetch all markdown files matching the glob in 'pattern'
     // from the specified npm package and serve them with a content identifier of 'react-component-docs':
