c97864f081c1babb4e789452f53fb2fada7a2bb9
Erin Donehoo <105813956+edonehoo@users.noreply.github.com>
2025-11-13 15:04:26 -0500

    Update alert message

--- packages/documentation-framework/components/navAnnouncementBanner/navAnnouncementBanner.js
+++ packages/documentation-framework/components/navAnnouncementBanner/navAnnouncementBanner.js
@@ -16,11 +16,10 @@ export const NavAnnouncementBanner = () => {
       <Alert
         variant="info"
         isInline
-        title="We've redesigned our navigation"
         actionClose={<AlertActionCloseButton onClose={closeBanner} />}
         className="ws-nav-announcement-banner"
       >
-        Learn more about the changes on <a href="https://medium.com/patternfly" target="_blank" rel="noopener noreferrer">Medium</a> and share your feedback in our <a href="https://www.feedback.redhat.com/jfe/form/SV_9MKBjq8H7muINMy" target="_blank" rel="noopener noreferrer">user survey</a>.
+        We've redesigned our website's navigation menu to make it easier to find the information you need. Learn more about the changes on <a href="https://medium.com/patternfly" target="_blank" rel="noopener noreferrer">Medium</a> and share any thoughts in our <a href="https://www.feedback.redhat.com/jfe/form/SV_9MKBjq8H7muINMy" target="_blank" rel="noopener noreferrer">user feedback survey</a>.
       </Alert>
     </div>
   );
