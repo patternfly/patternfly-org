e3ce64509b3bcc65a3e68998ca027cd96d2235df
Austin Sullivan <ausulliv@redhat.com>
2026-02-09 14:28:47 -0500

    Add accordion

--- packages/documentation-site/patternfly-docs/content/foundations-and-styles/styles/motion/Animations/examples/Animations.tsx
+++ packages/documentation-site/patternfly-docs/content/foundations-and-styles/styles/motion/Animations/examples/Animations.tsx
@@ -66,7 +66,11 @@ import {
   NotificationDrawerList,
   NotificationDrawerListItem,
   NotificationDrawerListItemBody,
-  NotificationDrawerListItemHeader
+  NotificationDrawerListItemHeader,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionToggle
 } from '@patternfly/react-core';
 import { Table, Thead, Tbody, Tr, Th, Td, ExpandableRowContent } from '@patternfly/react-table';
 import SkeletonTable from '@patternfly/react-component-groups/dist/dynamic/SkeletonTable';
@@ -132,6 +136,120 @@ const EmptyStateLogoIcon: React.FunctionComponent = () => (
 const mainContainerPageId = 'main-content-page-layout-default-nav';
 const expandableColumns = ['Applications', 'Server', 'Branch', 'Status'];
 const initialExpandedServerNames = ['Cost Management']; // Default to expanded
+
+export const AccordionSingleExpandBehavior: React.FunctionComponent = ({ children }) => {
+  const [expanded, setExpanded] = useState();
+
+  const onToggle = (id: string) => {
+    if (id === expanded) {
+      setExpanded('');
+    } else {
+      setExpanded(id);
+    }
+  };
+
+  return (
+    <Accordion asDefinitionList={false}>
+      <AccordionItem isExpanded={expanded === 'ex-toggle1'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle1');
+          }}
+          id="ex-toggle1"
+        >
+          Item one
+        </AccordionToggle>
+        <AccordionContent id="ex-expand1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem isExpanded={expanded === 'ex-toggle2'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle2');
+          }}
+          id="ex-toggle2"
+        >
+          Item two
+        </AccordionToggle>
+        <AccordionContent id="ex-expand2">
+          <p>
+            Vivamus et tortor sed arcu congue vehicula eget et diam. Praesent nec dictum lorem. Aliquam id diam
+            ultrices, faucibus erat id, maximus nunc.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem isExpanded={expanded === 'ex-toggle3'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle3');
+          }}
+          id="ex-toggle3"
+        >
+          Item three
+        </AccordionToggle>
+        <AccordionContent id="ex-expand3">
+          <p>Morbi vitae urna quis nunc convallis hendrerit. Aliquam congue orci quis ultricies tempus.</p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem isExpanded={expanded === 'ex-toggle4'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle4');
+          }}
+          id="ex-toggle4"
+        >
+          Item four
+        </AccordionToggle>
+        <AccordionContent id="ex-expand4">
+          <p>
+            Donec vel posuere orci. Phasellus quis tortor a ex hendrerit efficitur. Aliquam lacinia ligula pharetra,
+            sagittis ex ut, pellentesque diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+            cubilia Curae; Vestibulum ultricies nulla nibh. Etiam vel dui fermentum ligula ullamcorper eleifend non quis
+            tortor. Morbi tempus ornare tempus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Mauris et velit neque. Donec ultricies condimentum mauris, pellentesque imperdiet libero
+            convallis convallis. Aliquam erat volutpat. Donec rutrum semper tempus. Proin dictum imperdiet nibh, quis
+            dapibus nulla. Integer sed tincidunt lectus, sit amet auctor eros.
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem isExpanded={expanded === 'ex-toggle5'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle5');
+          }}
+          id="ex-toggle5"
+        >
+          Item five
+        </AccordionToggle>
+        <AccordionContent id="ex-expand5">
+          <p>Vivamus finibus dictum ex id ultrices. Mauris dictum neque a iaculis blandit.</p>
+        </AccordionContent>
+      </AccordionItem>
+      {children && (<AccordionItem isExpanded={expanded === 'ex-toggle6'}>
+        <AccordionToggle
+          onClick={() => {
+            onToggle('ex-toggle6');
+          }}
+          id="ex-toggle6"
+        >
+          Nested accordion
+        </AccordionToggle>
+        <AccordionContent id="ex-expand6">
+          {children}
+        </AccordionContent>
+      </AccordionItem>)}
+    </Accordion>
+  );
+};
+
 
 const AnimationsPage: FunctionComponent = () => {
   const drawerRef = useRef<HTMLElement | null>(null);
@@ -354,6 +472,7 @@ const AnimationsPage: FunctionComponent = () => {
     return Object.values(isUnreadMap).reduce((count, value) => count + (value ? 1 : 0), 0);
   };
 
+
   return (
     <Page
       masthead={
@@ -567,6 +686,7 @@ const AnimationsPage: FunctionComponent = () => {
           <Tab eventKey={0} title={<TabTitleText>Overview</TabTitleText>} tabContentId="overview" />
           <Tab eventKey={1} title={<TabTitleText>Resources</TabTitleText>} tabContentId="resources" />
           <Tab eventKey={2} title={<TabTitleText>Database</TabTitleText>} tabContentId="database" />
+          <Tab eventKey={3} title={<TabTitleText>Other components</TabTitleText>} tabContentId="other-components" />
         </Tabs>
       </PageSection>
       {selectedTab === 0 && (
@@ -597,33 +717,40 @@ const AnimationsPage: FunctionComponent = () => {
           )}
         </PageSection>
       )}
+      {selectedTab === 3 && (
+        <PageSection>
+          <AccordionSingleExpandBehavior>
+            <AccordionSingleExpandBehavior />
+          </AccordionSingleExpandBehavior>
+        </PageSection>
+      )}
       <Modal
         isOpen={showWizardModal}
         variant={ModalVariant.medium}
       >
-          <Wizard
-            height={400}
-            title="Header wizard"
-            onClose={() => setShowWizardModal(false)}
-            header={
-              <WizardHeader
-                title="You're a wizard, Harry"
-                description="To be clear, all those not named Harry are not wizards."
-                closeButtonAriaLabel="Close header"
-                onClose={() => setShowWizardModal(false)}
-              />
-            }
-          >
-            <WizardStep name="Step 1" id="header-first-step">
-              Step 1 content
-            </WizardStep>
-            <WizardStep name="Step 2" id="header-second-step">
-              Step 2 content
-            </WizardStep>
-            <WizardStep name="Review" id="header-review-step" footer={{ nextButtonText: 'Finish' }}>
-              Review step content
-            </WizardStep>
-          </Wizard>
+        <Wizard
+          height={400}
+          title="Header wizard"
+          onClose={() => setShowWizardModal(false)}
+          header={
+            <WizardHeader
+              title="You're a wizard, Harry"
+              description="To be clear, all those not named Harry are not wizards."
+              closeButtonAriaLabel="Close header"
+              onClose={() => setShowWizardModal(false)}
+            />
+          }
+        >
+          <WizardStep name="Step 1" id="header-first-step">
+            Step 1 content
+          </WizardStep>
+          <WizardStep name="Step 2" id="header-second-step">
+            Step 2 content
+          </WizardStep>
+          <WizardStep name="Review" id="header-review-step" footer={{ nextButtonText: 'Finish' }}>
+            Review step content
+          </WizardStep>
+        </Wizard>
       </Modal>
     </Page>
   );
