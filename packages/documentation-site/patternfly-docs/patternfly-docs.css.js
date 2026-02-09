7a2727c012072bb55b270e932ee0d73492eca641
Michael Coker <mcoker@redhat.com>
2026-02-09 11:46:51 -0600

    chore: hack sesh updates

--- packages/documentation-site/patternfly-docs/patternfly-docs.css.js
+++ packages/documentation-site/patternfly-docs/patternfly-docs.css.js
@@ -4,6 +4,7 @@ import '@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css';
 
 // Patternfly
 import '@patternfly/patternfly/patternfly.css';
+import './custom-org.css';
 // Patternfly utilities
 import '@patternfly/patternfly/patternfly-addons.css';
 // Global theme CSS
