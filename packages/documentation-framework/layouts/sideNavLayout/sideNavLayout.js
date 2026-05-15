ea4eca2c1bf83880b2211baeb79f0762c7eac135
Eric Olkowski <git.eric@thatblindgeye.dev>
2026-05-15 09:46:23 -0400

    fix(Cookies/GDPR): moved location in DOM for keyboard a11y

--- packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
+++ packages/documentation-framework/layouts/sideNavLayout/sideNavLayout.js
@@ -350,6 +350,10 @@ export const SideNavLayout = ({ children, groupedRoutes, navOpen: navOpenProp })
   return (
     <React.Fragment>
       <RtlContext.Provider value={isRTL}>
+        <div id="ws-page-banners">
+          <NavAnnouncementBanner />
+          {hasGdprBanner && <GdprBanner />}
+        </div>
         <Page
           id="ws-page"
           mainContainerId="ws-page-main"
@@ -363,10 +367,6 @@ export const SideNavLayout = ({ children, groupedRoutes, navOpen: navOpenProp })
           {children}
           {process.env.hasFooter && <Footer isDarkTheme={isDarkTheme} />}
         </Page>
-        <div id="ws-page-banners">
-          <NavAnnouncementBanner />
-          {hasGdprBanner && <GdprBanner />}
-        </div>
       </RtlContext.Provider>
     </React.Fragment>
   );
